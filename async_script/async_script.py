94007f162e1f7a80baccbe3dff44c31dc06cb4a6
TheG1uy <paulnvidia@gmail.com>
2018-11-05 14:16:31 +0200

    some fix

--- async_script/async_script.py
+++ async_script/async_script.py
@@ -12,8 +12,9 @@ from openvino.inference_engine import IENetwork, IEPlugin
 
 def build_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", help = "Path to an .xml file with a trained model.", required=True, type=str)
-    parser.add_argument("-i", "--input", help = "Path to a folder with images or path to an image files", required=True,
+    parser.add_argument("-m", "--model", help = "Path to an .xml file with a trained model.", required = True, type = str)
+    parser.add_argument("-w", "--weights", help = "Path to an .bin file with a trained weights.", required = True, type = str)
+    parser.add_argument("-i", "--input", help = "Path to a folder with images or path to an image files", required = True,
                         type = str, nargs = "+")
     parser.add_argument("-l", "--cpu_extension", 
                         help = "MKLDNN (CPU)-targeted custom layers.Absolute path to a shared library with the kernels "
@@ -30,18 +31,46 @@ def build_parser():
 
     return parser
 
+def image_convert(model, data):
+    n = net.inputs[next(iter(net.inputs))][0]
+    images = np.ndarray(shape = (net.inputs[next(iter(net.inputs))]))
+    for i in range(n):
+        image = cv2.imread(image_list[i])
+        if (image.shape[:-1] != (h,w)):
+                log.warning("Image {} is resized from {} to {}.".format(os.path.split(image_list[i])[1], 
+                             image.shape[:-1], (h,w)))
+                image = cv2.resize(image, (h, w))    	
+        image = image.transpose((2, 0, 1))
+        images[i] = image
+    return images
+
+def data_preparation(model, path):
+    if os.path.isdir(path[0]):
+        date = [path[0] + file for file in os.listdir(path[0])]
+    else:
+        data = path
+    net.batch_size = len(data)
+    video = {".mp4" : 1, ".avi" : 2, ".mvo" : 3, ".mpeg" : 4}
+    image = {".jpg" : 1, ".png" : 2, ".bmp" : 3, ".gif" : 4}
+    file = str(os.path.splitext(data[0])[1])
+    if file in image:
+        prep_data = image_convert(model, data)
+    images = []
+
+    return prep_data
+
 def main():
     log.basicConfig(format = "[ %(levelname)s ] %(message)s", level = log.INFO, stream = sys.stdout)
     args = build_parser().parse_args()
     model_xml = args.model
-    model_bin = os.path.splitext(model_xml)[0] + ".bin"
+    model_bin = args.weights
     log.info("Loading network files:\n\t {}\n\t {}".format(os.path.split(model_xml)[1], os.path.split(model_bin)[1]))
     net = IENetwork.from_ir(model_xml, model_bin)
     log.info("Preparing input blobs.")
     input_blob = next(iter(net.inputs))
     out_blob = next(iter(net.outputs))
     image_list = []
-
+    data = data_preparation(net, args.input)
     if os.path.isdir(args.input[0]):
         net.batch_size = len(os.listdir(args.input[0]))
         image_list = [args.input[0] + img for img in os.listdir(args.input[0])]
