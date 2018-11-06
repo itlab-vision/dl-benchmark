d989f8c68792db0df6f6f418b8fd362b9ce714ab
TheG1uy <paulnvidia@gmail.com>
2018-11-06 11:42:44 +0300

    Splited on six func

--- async_script/async_script.py
+++ async_script/async_script.py
@@ -16,6 +16,8 @@ def build_parser():
     parser.add_argument("-w", "--weights", help = "Path to an .bin file with a trained weights.", required = True, type = str)
     parser.add_argument("-i", "--input", help = "Path to a folder with images or path to an image files", required = True,
                         type = str, nargs = "+")
+    parser.add_argument("-r", "--Requests", help = "A positive integer value of infer requests to be created. Number of infer"
+                         "requests may be limited by device capabilities", required = True, type = int)
     parser.add_argument("-l", "--cpu_extension", 
                         help = "MKLDNN (CPU)-targeted custom layers.Absolute path to a shared library with the kernels "
                             "impl.", type = str, default = None)
@@ -31,77 +33,69 @@ def build_parser():
 
     return parser
 
-def image_convert(model, data):
-    n = net.inputs[next(iter(net.inputs))][0]
-    images = np.ndarray(shape = (net.inputs[next(iter(net.inputs))]))
+def model_preparation(log):
+    args = build_parser().parse_args()
+    model_xml = args.model
+    model_bin = args.weights
+    log.info("Plugin initialization.");
+    plugin = IEPlugin(device = args.device, plugin_dirs = args.plugin_dir)
+    if args.cpu_extension and 'CPU' in args.device:
+        plugin.add_cpu_extension(args.cpu_extension)
+    log.info("Loading network files:\n\t {}\n\t {}".format(os.path.split(model_xml)[1], os.path.split(model_bin)[1]))
+    net = IENetwork.from_ir(model_xml, model_bin)
+    if "CPU" in plugin.device:
+        supported_layers = plugin.get_supported_layers(net)
+        not_supported_layers = [l for l in net.layers.keys() if l not in supported_layers]
+        if len(not_supported_layers) != 0:
+            log.error("Following layers are not supported by the plugin for specified device {}:\n {}".
+                      format(plugin.device, ', '.join(not_supported_layers)))
+            log.error("Please try to specify cpu extensions library path in sample's command line parameters using -l "
+                      "or --cpu_extension command line argument")
+            sys.exit(1)      
+    if os.path.isdir(args.input[0]):
+        data = [args.input[0] + file for file in os.listdir(args.input[0])]
+    else:
+        data = args.input
+
+    return net, plugin, data, args
+
+
+def image_convert(model, data, log):
+    n, c, h, w  = model.inputs[next(iter(model.inputs))]
+    images = np.ndarray(shape = (model.inputs[next(iter(model.inputs))]))
     for i in range(n):
-        image = cv2.imread(image_list[i])
+        image = cv2.imread(data[i])
         if (image.shape[:-1] != (h,w)):
-                log.warning("Image {} is resized from {} to {}.".format(os.path.split(image_list[i])[1], 
+                log.warning("Image {} is resized from {} to {}.".format(os.path.split(data[i])[1], 
                              image.shape[:-1], (h,w)))
                 image = cv2.resize(image, (h, w))    	
         image = image.transpose((2, 0, 1))
         images[i] = image
     return images
 
-def data_preparation(model, path):
-    if os.path.isdir(path[0]):
-        date = [path[0] + file for file in os.listdir(path[0])]
-    else:
-        data = path
-    net.batch_size = len(data)
+def video_convert(model, data, log):
+    n, c, h, w  = model.inputs[next(iter(model.inputs))]
+    return n    
+
+def data_preparation(model, data, log):
     video = {".mp4" : 1, ".avi" : 2, ".mvo" : 3, ".mpeg" : 4}
     image = {".jpg" : 1, ".png" : 2, ".bmp" : 3, ".gif" : 4}
     file = str(os.path.splitext(data[0])[1])
     if file in image:
-        prep_data = image_convert(model, data)
-    images = []
-
+        prep_data = image_convert(model, data, log)
     return prep_data
 
-def main():
-    log.basicConfig(format = "[ %(levelname)s ] %(message)s", level = log.INFO, stream = sys.stdout)
-    args = build_parser().parse_args()
-    model_xml = args.model
-    model_bin = args.weights
-    log.info("Loading network files:\n\t {}\n\t {}".format(os.path.split(model_xml)[1], os.path.split(model_bin)[1]))
-    net = IENetwork.from_ir(model_xml, model_bin)
-    log.info("Preparing input blobs.")
-    input_blob = next(iter(net.inputs))
-    out_blob = next(iter(net.outputs))
-    image_list = []
-    data = data_preparation(net, args.input)
-    if os.path.isdir(args.input[0]):
-        net.batch_size = len(os.listdir(args.input[0]))
-        image_list = [args.input[0] + img for img in os.listdir(args.input[0])]
-    else:
-        net.batch_size = len(args.input)
-        image_list = args.input
-    log.info("Batch size is {}.".format(net.batch_size))    
-    n, c, h, w = net.inputs[input_blob]
-    images = np.ndarray(shape = (n, c, h, w))
-
-    for i in range(n):
-        image = cv2.imread(image_list[i])
-        if (image.shape[:-1] != (h,w)):
-                log.warning("Image {} is resized from {} to {}.".format(os.path.split(image_list[i])[1], 
-                             image.shape[:-1], (h,w)))
-                image = cv2.resize(image, (h, w))    	
-        image = image.transpose((2, 0, 1))
-        images[i] = image
-    log.info("Plugin initialization.");
-    plugin = IEPlugin(device = args.device, plugin_dirs = args.plugin_dir)
-    log.info("Loading model to the plugin")
-    exec_net = plugin.load(network = net)
-    del net
-
-    log.info("Starting inference ({} iterations)".format(args.number_iter))
+def start_infer_async(images, exec_net, model,  args):
+    input_blob = next(iter(model.inputs))
     for i in range(args.number_iter):
         infer_request_handle = exec_net.start_async(request_id = 0, inputs = {input_blob: images})
         infer_status = infer_request_handle.wait()
 
     log.info("Processing output blob")
-    res = infer_request_handle.outputs[out_blob]
+    res = infer_request_handle.outputs[next(iter(model.outputs))]
+    return res
+
+def infer_output(res, data, args, log):
     log.info("Top {} results: \n".format(args.number_top))
     if not args.labels:
     	args.labels = "image_net_synset.txt"
@@ -111,12 +105,23 @@ def main():
     for i, probs in enumerate(res):
         probs = np.squeeze(probs)
         top_ind = np.argsort(probs)[-args.number_top:][::-1]
-        print("Image {}\n".format(os.path.split(image_list[i])[1]))
+        print("Image {}\n".format(os.path.split(data[i])[1]))
         for id in top_ind:
             det_label = labels_map[id] if labels_map else "#{}".format(id)
             print("{:.7f} {}".format(probs[id], det_label))
-        print("\n")
+        print("\n")   
 
+def main():
+    log.basicConfig(format = "[ %(levelname)s ] %(message)s", level = log.INFO, stream = sys.stdout)
+    net, plugin, data, args = model_preparation(log)
+    net.batch_size = len(data)
+    images = data_preparation(net, data, log)
+    log.info("Loading model to the plugin")
+    exec_net = plugin.load(network = net, num_requests = args.Requests)
+    log.info("Starting inference ({} iterations)".format(args.number_iter))
+    res = start_infer_async(images, exec_net, net, args)
+    del net
+    infer_output(res, data, args, log)
     del exec_net
     del plugin
 
