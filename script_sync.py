fa6ca7cc71f17c816c4ab368cbdd1b96a573c499
Roman Rybkin <roma-roma97@mail.ru>
2018-11-21 20:18:59 +0300

    add out segmentation

--- script_sync.py
+++ script_sync.py
@@ -13,7 +13,7 @@ def build_argparser():
     parser.add_argument("-m", "--model", help = "Path to an .xml \
         file with a trained model.", required = True, type = str)
     parser.add_argument("-w", "--weights", help = "Path to an .bin file \
-        with a trained weights.", required = True, type = str)
+        with a trained weights.",default=None, type = str)
     parser.add_argument("-i", "--input", help = "Path to a folder with \
         images or path to an image files", required = True, type = str, nargs = "+")
     parser.add_argument("-l", "--cpu_extension", help = "MKLDNN \
@@ -33,6 +33,8 @@ def build_argparser():
         iterations", default = 1, type = int)
     parser.add_argument("-pc", "--perf_counts", help = "Report performance \
         counters", action = "store_true", default = False)
+    parser.add_argument("-t", "--type_model", help = "Type model", required = True, 
+        type = str)
     return parser
 
 def Convert_Image(net, args, log):
@@ -50,7 +52,10 @@ def Convert_Image(net, args, log):
 def Model_loading(log):
     args=build_argparser().parse_args()
     model_xml=args.model
-    model_bin =args.weights
+    if args.weights:
+        model_bin =args.weights
+    else:
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
     plugin = IEPlugin(device=args.device, plugin_dirs=args.plugin_dir)
     if args.cpu_extension and 'CPU' in args.device:
         plugin.add_cpu_extension(args.cpu_extension)
@@ -83,23 +88,30 @@ def Inference_sync(net,plugin,images,args,log):
     return (res, infer_time)
 
 def Inference_out_classification(res, net, args, log):
-    log.info("Top {} results: ".format(args.number_top))
-    if args.labels:
-        with open(args.labels, 'r') as f:
-            labels_map = [x.split(sep=' ', maxsplit=1)[-1].strip() for x in f]
-    else:
-        labels_map = None
-        for i, probs in enumerate(res):
-            probs = np.squeeze(probs)
-            top_ind = np.argsort(probs)[-args.number_top:][::-1]
-            print("Image {}\n".format(args.input[i]))
-            for id in top_ind:
-                det_label = labels_map[id] if labels_map else "#{}".format(id)
-                print("{:.7f} label {}".format(probs[id], det_label))
-            print("\n")
+	log.info("Top {} results: ".format(args.number_top))
+	if args.labels:
+		with open(args.labels, 'r') as f:
+			labels_map = [x.split(sep=' ', maxsplit=1)[-1].strip() for x in f]
+	else:
+		labels_map=None
+	for i, probs in enumerate(res):
+		probs = np.squeeze(probs)
+		top_ind = np.argsort(probs)[-args.number_top:][::-1]
+		print("Image {}\n".format(args.input[i]))
+		for id in top_ind:
+			det_label = labels_map[id] if labels_map else "#{}".format(id)
+			print("{:.7f} label {}".format(probs[id], det_label))
+		print("\n")
+def Output(res, net, args, log):
+	log.info("Start output.")
+	if args.type_model=="c":
+		Inference_out_classification(res, net, args, log)
+	elif args.type_model=="s":
+		Inference_out_segmentation(res, net, args, log)
 
-def Inference_out_segmentation(res, net, log):
-    classes_color_map = [
+
+def Inference_out_segmentation(res, net, args, log):
+	classes_color_map = [
     (150, 150, 150),
     (58, 55, 169),
     (211, 51, 17),
@@ -121,31 +133,28 @@ def Inference_out_segmentation(res, net, log):
     (50, 248, 83),
     (178, 101, 130),
     (71, 30, 204)
-                        ]
-    n, c, h, w = net.inputs[next(iter(net.inputs))]                 
-    for batch, data in enumerate(res):
-        classes_map = np.zeros(shape=(h, w, c), dtype=np.int)
-        for i in range(h):
-            for j in range(w):
-                if len(data[:, i, j]) == 1:
-                    pixel_class = int(data[:, i, j])
-                else:
-                    pixel_class = np.argmax(data[:, i, j])
-                    classes_map[i, j, :] = classes_color_map[min(pixel_class, 20)]
-        out_img = os.path.join(os.path.dirname(__file__), "out_{}.bmp".format(batch))
-        cv2.imwrite(out_img, classes_map)
-        log.info("Result image was saved to {}".format(out_img))
+						]
+	n, c, h, w = net.inputs[next(iter(net.inputs))]					
+	for batch, data in enumerate(res):
+		classes_map = np.zeros(shape=(len(data[0]), len(data[0]), c), dtype=np.int)
+		for i in range(len(data[0])):
+			for j in range(len(data[0])):
+				if len(data[:, i, j]) == 1:
+					pixel_class = int(data[:, i, j])
+				else:
+					pixel_class = np.argmax(data[:, i, j])
+				classes_map[i, j, :] = classes_color_map[min(pixel_class, 20)]
+		out_img = os.path.join(os.path.dirname(__file__), "out_segmentation_{}.bmp".format(batch))
+		cv2.imwrite(out_img, classes_map)
+		log.info("Result image was saved to {}".format(out_img))
 
 def main():
-    log.basicConfig(format = "[ %(levelname)s ] %(message)s",
-    level = log.INFO, stream = sys.stdout)
-    net, plugin, args = Model_loading(log)
-    images=Convert_Image(net, args, log)
-    res, time=Inference_sync(net, plugin, images, args, log)
-    Inference_out_classification(res, net, args, log)
-    print(time)
-    #Inference_out_segmentation(res, net, log)
-    
+	log.basicConfig(format = "[ %(levelname)s ] %(message)s",
+	level = log.INFO, stream = sys.stdout)
+	net, plugin, args = Model_loading(log)
+	images=Convert_Image(net, args, log)
+	res, time=Inference_sync(net, plugin, images, args, log)
+	Output(res, net, args, log)
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
