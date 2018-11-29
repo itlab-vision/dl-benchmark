3ec274443f79157dfd7f9fdb2dbb39aadb1e3c5a
KchnKchn <khvatov.alexander@gmail.com>
2018-11-29 12:31:52 +0300

    Division into files

--- src/test_inference.py
+++ src/test_inference.py
@@ -1,89 +1,23 @@
-import os
 import argparse
-from lxml import etree
-
-class Model:
-    def __init__(self, model):
-        self.name = model[0]
-        self.path = model[1]
-
-class Dataset:
-    def __init__(self, dataset):
-        self.name = dataset[0]
-        self.path = dataset[1]
-
-class Parameters:
-    def __init__(self, parameters):
-        self.batchsize = parameters[0]
-        self.mode = parameters[1]
-        self.plugin = parameters[2]
-        self.asyncrequest = parameters[3]
-        self.synciteration = parameters[4]
-        self.mininferencetime = parameters[5]
-
-class Test:
-    def __init__(self, args):
-        self.model = args[0]
-        self.dataset = args[1]
-        self.parameters = args[2]
-        
+import readconfig as rc
+     
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', type = str, dest = 'path_to_config',
-        help = 'Path to folder with config')
+        help = 'Path to file with config')
     
     config = parser.parse_args().path_to_config
-
-    if config is None:
-        config = os.path.join(os.getcwd(), 'config.xml')
-    else:
-        config = os.path.join(config, 'config.xml')
-    
+	
     if not os.path.isfile(config):
-        raise ValueError('Config not found!')
+        raise ValueError('Wrong path to file!')
     
     return config
 
-def process_config(config):
-    with open(config) as file:
-        openconfig = file.read()
-    
-    utf_parser = etree.XMLParser(encoding='utf-8')
-    root = etree.fromstring(openconfig.encode('utf-8'), parser = utf_parser)
-    
-    TestList = []
-    
-    for Tag in root.getchildren():
-    
-        tmp = []
-        
-        for TestParameter in Tag.getchildren():
-            childrens = []
-            for Parameter in TestParameter.getchildren():
-                childrens.append(Parameter.text)
-                
-            if TestParameter.tag == 'Model':
-                model = Model(childrens)
-                tmp.append(model)
-                
-            if TestParameter.tag == 'Dataset':
-                dataset = Dataset(childrens)
-                tmp.append(dataset)
-                
-            if TestParameter.tag == 'Parameters':
-                parameters = Parameters(childrens)
-                tmp.append(parameters)
-                
-        test = Test(tmp)
-        TestList.append(test)
-    
-    return TestList
-
-def test_inference(TestList):
+def test_inference(test_list):
     output_data = []
     
-    for i in range(len(TestList)):
-        if TestList[i].parameters.mode == 'Sync':
+    for i in range(len(test_list)):
+        if ((test_list[i].parameters.mode).lower()) == 'sync':
             #call sync test
             output_data.append([])
         else:
@@ -97,5 +31,5 @@ if __name__ == "__main__":
         config = build_parser()
     except Exception as Error:
         print('Error! : {}'.format(str(Error)))
-    TestList = process_config(config)
-    Output = test_inference(TestList)
+    test_list = rc.process_config(config)
+    output = test_inference(test_list)
