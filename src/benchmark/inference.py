854d253fb6845bc16d01fa4877682b2cdbe48770
KchnKchn <khvatov.alexander@gmail.com>
2018-12-11 19:44:06 +0300

    Need test

--- src/benchmark/inference.py
+++ src/benchmark/inference.py
@@ -10,30 +10,38 @@ sys.path.append(infer_path)
 import inference_async_mode as as_mode
 import inference_sync_mode as s_mode
 
-def test_async(model, dataset, params):
+def test_async(model, dataset, param):
     log.basicConfig(format = "[ %(levelname)s ] %(message)s",
         level = log.INFO, stream = sys.stdout)
     log.info("Start async inference test on model : {}".format(model.name))
-    net, plugin, data = as_mode.prepare_model(log, model.model, model.weigh,
-        None, params.plugin, None, dataset.path)
-    net.batch_size = params.batchsize
-    images = as_mode.prepare_data(net, data)
-    log.info("Loading model to the plugin")
-    exec_net = plugin.load(network = net, num_requests = params.asyncrequest)
-    log.info("Starting inference ({} iterations)".format(params.iteration))
-    _, time = as_mode.infer_async(images, exec_net, net, params.iteration)
-    log.info("End async inference test on model : {}".format(model.name))
-    return time
+    try:
+        net, plugin, data = as_mode.prepare_model(log, model.model,
+            model.weight, None, param.plugin, None, dataset.path)
+        net.batch_size = param.batchsize
+        images = as_mode.prepare_data(net, data)
+        log.info("Loading model to the plugin")
+        exec_net = plugin.load(network = net,
+            num_requests = param.asyncrequest)
+        log.info("Starting inference ({} iterations)".format(param.iteration))
+        _, time = as_mode.infer_async(images, exec_net, net, param.iteration)
+        log.info("End async inference test on model : {}".format(model.name))
+        return time
+    except Exception as ex:
+        log.warning("Async inference test was ended with error:")
+        print("{}".format(str(ex)))
 
-def test_sync(model, dataset, params):
+def test_sync(model, dataset, param):
     log.basicConfig(format = "[ %(levelname)s ] %(message)s",
         level = log.INFO, stream = sys.stdout)
     log.info("Start sync inference test on model : {}".format(model.name))
-    net, plugin = s_mode.prepare_model(model.model, model.weigh,
-        None, params.plugin, None, dataset.path, log)
-    print('Ok')
-    images = s_mode.convert_image(net, dataset.path, log)
-    print('Ok')
-    _, time = s_mode.infer_sync(net, plugin, images, params.iteration, log)
-    log.info("End sync inference test on model : {}".format(model.name))
-    return time
+    try:
+        net, plugin, data = s_mode.prepare_model(model.model, model.weight,
+            None, param.plugin, None, dataset.path, log)
+        net.batch_size = param.batchsize
+        images = s_mode.convert_image(net, data, log)
+        _, time = s_mode.infer_sync(net, plugin, images, param.iteration, log)
+        log.info("End sync inference test on model : {}".format(model.name))
+        return time
+    except Exception as ex:
+        log.warning("Sync inference test was ended with error:")
+        print("{}".format(str(ex)))
