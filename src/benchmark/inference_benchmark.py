d3d6c7275703710ca8c827fcb0406ac0c2e9e551
KchnKchn <khvatov.alexander@gmail.com>
2018-12-11 22:25:27 +0300

    Add output in file

--- src/benchmark/inference_benchmark.py
+++ src/benchmark/inference_benchmark.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import config_parser
 import inference
+import output
 
 def build_parser():
     parser = argparse.ArgumentParser()
@@ -15,21 +16,37 @@ def build_parser():
     return config
 
 def inference_benchmark(test_list):
+    table = []
     for i in range(len(test_list)):
         mode = (test_list[i].parameter.mode).lower()
+        latency = None
+        fps = None
+        time = None
+        average_time = None
+        
         if mode == 'sync':
             inference_time = inference.test_sync(test_list[i].model, 
                 test_list[i].dataset, test_list[i].parameter)
-        else:
+            #postprocessing time
+            #Latency
+            #FPS
+        
+        if mode == 'async':
             inference_time = inference.test_async(test_list[i].model,
                 test_list[i].dataset, test_list[i].parameter)
+            average_time = inference_time
+            #FPS
 
-    return inference_time
+        table_row = output.create_table_row(test_list[i].model, test_list[i].dataset,
+            test_list[i].parameter, average_time, latency, fps)
+        table.append(table_row)
+    return table
     
 if __name__ == "__main__":
     try:
         config = build_parser()
         test_list = config_parser.process_config(config)
-        output = inference_benchmark(test_list)
+        table = inference_benchmark(test_list)
+        output.save_table_in_file(table)
     except Exception as exp:
         print('Error! : {}'.format(str(exp)))
---
+++ src/benchmark/output.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+auxiliary_path = os.path.dirname(os.path.abspath(__file__))
+auxiliary_path = auxiliary_path[0:-9]
+auxiliary_path = os.path.join(auxiliary_path, 'auxiliary')
+sys.path.append(auxiliary_path)
+
+import node_info as info
+
+def create_table_row(model, dataset, param, average_time, latency, fps):
+    hardware = info.get_system_characteristics()
+    other_param = "Plugin: {}, Async request count: {}, Iteration count: {}, Min inference time: {}".format(param.plugin,
+        param.asyncrequest, param.iteration, param.mininferencetime)
+    table_row = "{};{};{};{};{};{};{};{};{};".format(model.name, dataset.name,
+        param.batchsize, param.mode, other_param, hardware, average_time,
+        latency, fps)
+    return table_row
+
+def save_table_in_file(table):
+    file = open("results.csv", "w")
+    head = "Model;Dataset;BatchSize;Mode;Parameters;Infrastucture;Average time of single pass (s);Latency;FPS"
+    file.write(head + "\n")
+    for line in range(len(table)):
+        file.write(table[line] + "\n")
+    file.close()
