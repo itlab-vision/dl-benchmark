2703c75266b6d89d72c8a1ca7005c056ab46e095
KchnKchn <khvatov.alexander@gmail.com>
2018-12-12 15:33:22 +0300

    Fix table_row formation

--- src/benchmark/output.py
+++ src/benchmark/output.py
@@ -10,10 +10,14 @@ import node_info as info
 
 def create_table_row(model, dataset, param, average_time, latency, fps):
     hardware = info.get_system_characteristics()
+    hardware_info = ""
+    for key in hardware:
+        hardware_info += "{}: {},".format(key, hardware[key])
+    hardware_info = hardware_info[:-1]
     other_param = "Plugin: {}, Async request count: {}, Iteration count: {}, Min inference time: {}".format(param.plugin,
         param.asyncrequest, param.iteration, param.mininferencetime)
     table_row = "{};{};{};{};{};{};{};{};{};".format(model.name, dataset.name,
-        param.batchsize, param.mode, other_param, hardware, average_time,
+        param.batchsize, param.mode, other_param, hardware_info, average_time,
         latency, fps)
     return table_row
 
