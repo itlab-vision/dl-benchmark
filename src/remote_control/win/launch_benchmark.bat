4f900aac7726dc611b3f3b5909942c73f1dbc02c
TheG1uy <paulnvidia@gmail.com>
2019-01-21 13:59:14 +0300

    remotely launched async script

--- src/remote_control/win/launch_benchmark.bat
+++ src/remote_control/win/launch_benchmark.bat
@@ -1,1 +1,8 @@
-@echo Start file>C:\\Users\\kumbrasev.p\\Documents\\GitHub\\openvino-dl-benchmark\\src\\remote_control\\win\\1.txt
+@echo off
+call "C:\Intel\computer_vision_sdk_2018.4.420\bin\setupvars.bat"
+set async_py="C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\src\inference\inference_async_mode.py"
+set "model=%C:\Intel\computer_vision_sdk_2018.4.420\deployment_tools\model_downloader\classification\alexnet\IR\alexnet.xml"
+set "weights=%C:\Intel\computer_vision_sdk_2018.4.420\deployment_tools\model_downloader\classification\alexnet\IR\alexnet.bin"
+set "images=%C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\data\ImageNET\"
+set "labels =%C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\src\inference\image_net_synset.txt"
+call python %async_py% -m %model% -w %weights% -i %images% -t classification -r 1 -b 1 -ni 10 -nt 5 >> C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\src\remote_control\win\result.txt
--- src/remote_control/win/remote_start.py
+++ src/remote_control/win/remote_start.py
@@ -1,8 +1,8 @@
 import wmi, time
 
 def get_process():
-    #c = wmi.WMI("ws-2k-110-06", user="Kumbrasev.p", password="Qwerty123")
-    c = wmi.WMI()
+    c = wmi.WMI("ws-2k-110-06", user="Kumbrasev.p", password="Qwerty123")
+    #c = wmi.WMI()
     process_startup = c.Win32_ProcessStartup.new()
     process_startup.ShowWindow = 1
     process_id, result = c.Win32_Process.Create(CommandLine="C:\\Users"
