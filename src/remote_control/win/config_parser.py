4c38b145ac9800c973dc68dc440d5fd92018974a
TheG1uy <paulnvidia@gmail.com>
2019-01-24 14:28:53 +0300

    now work on windows

---
+++ src/remote_control/win/config_parser.py
@@ -0,0 +1,29 @@
+from lxml import etree
+
+class machine:
+    def __init__(self, params):
+        self.ip = params[0]
+        self.login = params[1]
+        self.psw = params[2]
+        self.client_path = params[3]
+
+
+def parse_config(config):
+    with open(config) as file:
+        openconfig = file.read()
+    utf_parser = etree.XMLParser(encoding = 'utf-8')
+    root = etree.fromstring(openconfig.encode('utf-8'), parser = utf_parser)
+    machine_list = []
+    for machine_tag in root.getchildren():
+        machine_parameters = []
+        for machine_parameter in machine_tag.getchildren():
+            if machine_parameter.tag == 'Ip':
+                machine_parameters.append(machine_parameter.text)
+            if machine_parameter.tag == 'Login':
+                machine_parameters.append(machine_parameter.text)
+            if machine_parameter.tag == 'Password':
+                machine_parameters.append(machine_parameter.text)
+            if machine_parameter.tag == 'Client_Path':
+                machine_parameters.append(machine_parameter.text)
+        machine_list.append(machine(machine_parameters))
+    return machine_list
--- src/remote_control/win/remote_start.py
+++ src/remote_control/win/remote_start.py
@@ -1,23 +1,42 @@
-import wmi, time
+import wmi
+import os
+import sys
+import argparse
+import logging as log
+import config_parser
 
-def get_process():
-    c = wmi.WMI("ws-2k-110-06", user="Kumbrasev.p", password="Qwerty123")
-    #c = wmi.WMI()
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type = str, dest = 'config_path',
+        help = 'Path to configuration file', required = True)
+    config = parser.parse_args().config_path
+    if not os.path.isfile(config):
+        raise ValueError('Wrong path to configuration file!')
+    return config
 
-    process_startup = c.Win32_ProcessStartup.new()
-    process_startup.ShowWindow = 1
-    process_id, result = c.Win32_Process.Create(CommandLine=('CMD.EXE /c (python C:\\Users'
-        '\\kumbrasev.p\\Documents\\GitHub\\openvino-dl-benchmark\\src'
-        '\\remote_control\\win\\ftp_client.py -ip 10.0.16.80 -l Kumbrasev.p -p Qwerty123)'),
+def run_benchmark(machine):
+    wmi_con = wmi.WMI(machine.ip, user=machine.login, password=machine.psw)
+    process_startup = wmi_con.Win32_ProcessStartup.new()
+    process_id, result = wmi_con.Win32_Process.Create(CommandLine=('cmd.exe /c' 
+        ' python ' + machine.client_path + ' -ip ' + machine.ip + ' -l ' + 
+        machine.login + ' -p ' + machine.psw),
         ProcessStartupInformation=process_startup)
     if result == 0:
-        print("Process started successfully: %d" % process_id)
+        log.info('Process started successfully {}'.format(process_id))
     else:
-        raise(RuntimeError, "Problem creating process: %d" % result)
+        log.info('Problem creating process {}'.format(result))
 
-
-        
+def main():
+    log.basicConfig(format = '[ %(levelname)s ] %(message)s',
+        level = log.INFO, stream = sys.stdout)
+    config = build_parser()
+    log.info('Parsing config file')
+    machine_list = config_parser.parse_config(config)
+    for machine in machine_list:
+        log.info('Run benchmark on {} machine'.format(machine.ip))
+        run_benchmark(machine)
+    log.info('Benchmark run on all machines')
 
 
 if __name__ == '__main__':
-    get_process()
+    sys.exit(main() or 0)
