0aadf85f7eac0f861644ed8ba7b19a1b5f4701af
TheG1uy <paulnvidia@gmail.com>
2019-02-12 17:00:46 +0300

    fix table path

--- src/remote_control/launch_benchmark.bat
+++ src/remote_control/launch_benchmark.bat
@@ -4,5 +4,5 @@ call "C:\Intel\computer_vision_sdk_2018.4.420\bin\setupvars.bat"
 call cd C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\src\benchmark\
 set benchmark_py="inference_benchmark.py"
 set "config=%C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\src\benchmark\benchmark_configuration.xml"
-set "output_file=%C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\src\remote_control\win\result_table.csv"
+set "output_file=%C:\Users\kumbrasev.p\Documents\GitHub\openvino-dl-benchmark\src\remote_control\result_table.csv"
 call python %benchmark_py% -c %config% -f %output_file% 
