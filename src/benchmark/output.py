8cbe7c00671705b38ccd9a8a15b1c8f747843fbc
KchnKchn <khvatov.alexander@gmail.com>
2019-04-11 21:50:04 +0300

    add stream count paremeter for benchmark

--- src/benchmark/output.py
+++ src/benchmark/output.py
@@ -23,8 +23,8 @@ def create_table_row(model, dataset, param, average_time, latency, fps):
     for key in hardware:
         hardware_info += '{}: {}, '.format(key, hardware[key])
     hardware_info = hardware_info[:-2]
-    other_param = 'Plugin: {}, Async request count: {}, Iteration count: {}, Thread count: {}, Min inference time (s): {}'.format(param.plugin,
-        param.async_request, param.iteration, param.nthreads, param.min_inference_time)
+    other_param = 'Plugin: {}, Async request count: {}, Iteration count: {}, Thread count: {}, Stream count: {}, Min inference time (s): {}'.format(param.plugin,
+        param.async_request, param.iteration, param.nthreads, param.nstreams, param.min_inference_time)
     table_row = '{};{};{};{};{};{};{};{};{};{};'.format(model.name, model.datatype,
         dataset.name, param.batch_size, param.mode, other_param, hardware_info,
         average_time, latency, fps)
