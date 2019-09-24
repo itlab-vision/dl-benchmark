a23a8bfd8e2f2bc0d620b16d34bdfc6ef95644c6
TheG1uy <paulnvidia@gmail.com>
2019-09-24 19:47:06 +0300

    Added delete oldversion_dirs and client

--- src/bench_deploy/client.sh
+++ src/bench_deploy/client.sh
@@ -3,15 +3,6 @@
 echo "Update components"
 #apt-get update
 
-if [[ -z "$(ls /etc/ | grep vsftpd.conf)" ]]
-then
-    echo "Install/Update FTP-server"
-    apt-get install vsftpd
-    systemctl start vsftpd
-    systemctl enable vsftpd
-    cat ftp_server_settigs.txt > /etc/vsftpd.conf
-fi
-
 rm -r /home/itmm/inference_engine_samples_build
 rm -r /home/itmm/openvino_models
 
