b8c858885117b45ccd788cb857f5b323cd3d0d46
TheG1uy <paulnvidia@gmail.com>
2019-09-26 15:30:29 +0300

    Try to intall product on client

--- src/bench_deploy/client.sh
+++ src/bench_deploy/client.sh
@@ -3,6 +3,7 @@
 echo "Update components"
 #apt-get update
 
+echo "Delete old components"
 rm -r /home/itmm/inference_engine_samples_build
 rm -r /home/itmm/openvino_models
 
@@ -12,4 +13,20 @@ wget $1
 arch_name="$(ls | grep ".tgz")"
 tar -xvzf $arch_name
 cd "$(basename $arch_name .tgz)"
-sudo ./install.sh
+sed -i 's/=decline/=accept/' silent.cfg
+
+echo "Delete old openvino"
+sed -i 's/=install/=uninstall/' silent.cfg
+./install.sh --silent silent.cfg
+
+echo "Install old openvino"
+sed -i 's/=uninstall/=install/' silent.cfg
+./install.sh --silent silent.cfg
+
+cd /opt/intel/openvino/install_dependencies
+./install_openvino_dependencies.sh -E
+source /opt/intel/openvino/bin/setupvars.sh
+cd /opt/intel/openvino/deployment_tools/model_optimizer/install_prerequisites
+./install_prerequisites.sh
+cd /opt/intel/openvino/deployment_tools/demo
+./demo_squeezenet_download_convert_run.sh
