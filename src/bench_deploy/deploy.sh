b5fed56eeb4c61ca8b5b97400d6d0ac78a90165d
TheG1uy <paulnvidia@gmail.com>
2019-11-16 21:02:43 +0300

    Added source

--- src/bench_deploy/deploy.sh
+++ src/bench_deploy/deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 echo "Run client script"
-./client.sh $1
+sudo ./client.sh $1
 cd /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader
 source ~/Documents/benchmark/OpenVINO_env/bin/activate
 
 python3 -mpip install --user -r ./requirements.in
 
 echo "Download models"
-./downloader.py --all
+sudo ./downloader.py --all
+
+echo "Setting vars"
+source /opt/intel/openvino/bin/setupvars.sh
 
 echo "Convert to FP16"
-./converter.py --all --precisions=FP16
+sudo ./converter.py --all --precisions=FP16
 echo "Convert to FP32"
-./converter.py --all --precisions=FP32
+sudo ./converter.py --all --precisions=FP32
 echo "Convert to INT8"
-./converter.py --all --precisions=INT8
+sudo ./converter.py --all --precisions=INT8
