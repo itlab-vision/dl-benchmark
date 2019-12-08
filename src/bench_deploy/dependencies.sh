cbb696a84a7d11be0372f68860e2c9ae97e00dc5
TheG1uy <paulnvidia@gmail.com>
2019-12-08 21:00:18 +0300

    Add sudo to each dependency

--- src/bench_deploy/dependencies.sh
+++ src/bench_deploy/dependencies.sh
@@ -10,9 +10,9 @@ sudo rm -rf OpenVINO_env
 sudo python3 -m venv OpenVINO_env
 source OpenVINO_env/bin/activate
 
-yes | pip3 install PyYAML
-yes | pip3 install requests
-yes | pip3 install numpy
-yes | pip3 install networkx==2.3
-yes | pip3 install defusedxml
-yes | pip install protobuf==3.6.1
+yes | sudo pip3 install PyYAML
+yes | sudo pip3 install requests
+yes | sudo pip3 install numpy
+yes | sudo pip3 install networkx==2.3
+yes | sudo pip3 install defusedxml
+yes | sudo pip install protobuf==3.6.1
