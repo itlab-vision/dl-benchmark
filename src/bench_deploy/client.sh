9ca693266e4667da212353f548eae1258c0aa07c
TheG1uy <paulnvidia@gmail.com>
2019-12-08 22:11:50 +0300

    Rewrite sudo

--- src/bench_deploy/client.sh
+++ src/bench_deploy/client.sh
@@ -7,11 +7,11 @@ echo "Update components"
 yes | apt-get update
 
 echo "Delete old components"
-rm -r /home/itmm/inference_engine_samples_build
-rm -r /home/itmm/openvino_models
+echo $password | sudo -S rm -r /home/itmm/inference_engine_samples_build
+echo $password | sudo -S -r /home/itmm/openvino_models
 
 echo "Install python dependencies"
-echo $password | sudo -S yes | ./dependencies.sh $password
+echo $password | sudo -S ./dependencies.sh $password
 
 source ~/Documents/benchmark/OpenVINO_env/bin/activate
 
@@ -25,22 +25,22 @@ sed -i 's/=decline/=accept/' silent.cfg
 
 echo "Delete old openvino"
 sed -i 's/=install/=uninstall/' silent.cfg
-echo $password | sudo -S yes | ./install.sh --silent silent.cfg
+echo $password | sudo -S ./install.sh --silent silent.cfg
 
 echo "Install new openvino"
 sed -i 's/=uninstall/=install/' silent.cfg
-echo $password | sudo -S yes | ./install.sh --silent silent.cfg
+echo $password | sudo -S ./install.sh --silent silent.cfg
 
 echo "Install openvino dependencies"
 cd /opt/intel/openvino/install_dependencies
-echo $password | sudo -S yes | ./install_openvino_dependencies.sh -E
+echo $password | sudo -S ./install_openvino_dependencies.sh -E
 
 echo "Setting vars"
 source /opt/intel/openvino/bin/setupvars.sh
 
 echo "Configurate model_optimizer"
 cd /opt/intel/openvino/deployment_tools/model_optimizer/install_prerequisites
-echo $password | sudo -S yes | ./install_prerequisites.sh
+echo $password | sudo -S ./install_prerequisites.sh
 
 echo "Test run"
 cd /opt/intel/openvino/deployment_tools/demo
