d02959d15609757e6aa0494faafba1cda61287ce
TheG1uy <paulnvidia@gmail.com>
2019-12-11 20:50:38 +0300

    Add status checker on client

--- src/bench_deploy/client.sh
+++ src/bench_deploy/client.sh
@@ -3,6 +3,9 @@
 dowload_link=$1
 password=$2
 
+#Installation status
+status=0
+
 echo "Update components"
 yes | apt-get update
 
@@ -30,10 +33,12 @@ echo $password | sudo -S ./install.sh --silent silent.cfg
 echo "Install new openvino"
 sed -i 's/=uninstall/=install/' silent.cfg
 echo $password | sudo -S ./install.sh --silent silent.cfg
+if [ $status -eq 0 ]; then status=$? fi
 
 echo "Install openvino dependencies"
 cd /opt/intel/openvino/install_dependencies
 echo $password | sudo -S ./install_openvino_dependencies.sh -E
+if [ $status -eq 0 ]; then status=$? fi
 
 echo "Setting vars"
 source /opt/intel/openvino/bin/setupvars.sh
@@ -41,10 +46,12 @@ source /opt/intel/openvino/bin/setupvars.sh
 echo "Configurate model_optimizer"
 cd /opt/intel/openvino/deployment_tools/model_optimizer/install_prerequisites
 echo $password | sudo -S ./install_prerequisites.sh
+if [ $status -eq 0 ]; then status=$? fi
 
 echo "Test run"
 cd /opt/intel/openvino/deployment_tools/demo
 ./demo_squeezenet_download_convert_run.sh
+if [ $status -eq 0 ]; then status=$? fi
 
 # cd /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader
 # source ~/Documents/benchmark/OpenVINO_env/bin/activate
@@ -53,15 +60,19 @@ cd /opt/intel/openvino/deployment_tools/demo
 
 # echo "Download models"
 # sudo ./downloader.py --all
+# if [ $status -eq 0 ]; then status=$? fi
 
 # echo "Setting vars"
 # source /opt/intel/openvino/bin/setupvars.sh
 
 # echo "Convert to FP16"
 # sudo ./converter.py --all --precisions=FP16 --mo ../../../model_optimizer/mo.py
+# if [ $status -eq 0 ]; then status=$? fi
 # echo "Convert to FP32"
 # sudo ./converter.py --all --precisions=FP32 --mo ../../../model_optimizer/mo.py
+# if [ $status -eq 0 ]; then status=$? fi
 # echo "Convert to INT8"
 # sudo ./converter.py --all --precisions=INT8 --mo ../../../model_optimizer/mo.py
+# if [ $status -eq 0 ]; then status=$? fi
 
-echo "InstallSuccess"
+if [ $status -eq 0 ]; then echo "InstallSuccess" fi
