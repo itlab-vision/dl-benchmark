26b288e9aa8196d37795fd75363ac3c7d6414fd8
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-04-08 21:46:28 +0300

    combo boxx activated template, widgettablesettings template

--- gui/widgets.py
+++ gui/widgets.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QTableWidget, QGridLayout, QComboBox
+from PyQt5.QtWidgets import QWidget, QTableWidget, QGridLayout, QComboBox, QLabel
 
 
 class WidgetData(QWidget):
@@ -7,9 +7,31 @@ class WidgetData(QWidget):
         self.initUI()
 
     def initUI(self):
-        grid = QGridLayout()
-        self.setLayout(grid)
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
         self.menu = QComboBox()
         self.menu.addItems(['Управление моделями', 'Управление данными', 'Управление тестами'])
-        grid.addWidget(self.menu, 0, 0)
-        self.setLayout(grid)
+        self.menu.activated[str].connect(self.onActivated)
+        self.grid.addWidget(self.menu, 0, 0)
+        self.setLayout(self.grid)
+
+    def onActivated(self, type):
+        if type == 'Управление моделями':
+            ...
+        elif type == 'Управление данными':
+            ...
+        elif type == 'Управление тестами':
+            ...
+
+
+class WidgetTableSetting(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.column_count = None
+        self.row_count = 1
+        self.initUI()
+
+    def initUI(self):
+        self.table = QTableWidget(self)
+        self.table.setColumnCount((self.column_count))
+        self.table.setRowCount((self.row_count))
