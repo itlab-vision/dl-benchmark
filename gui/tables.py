3b037454f48baf75e1ef78c5537264d98beafd9b
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-04-12 13:31:02 +0300

    add classes model, data, def add object, update table

--- gui/tables.py
+++ gui/tables.py
@@ -1,16 +1,17 @@
 from PyQt5.QtWidgets import *
+from objects import *
 
 
 class TableModel(QTableWidget):
     def __init__(self):
         super().__init__()
-        self._model = []
-        self._headers = ['Название модели', 'Абсолютный путь']
-        self._count_col = 2
+        self._models = []
+        self._count_col = 3
         self._count_row = 50
-        self.setHorizontalHeaderLabels(self._headers)
+        self._headers = ["Название модели", "Тип модели", "Абсолютный путь"]
         self.setColumnCount(self._count_col)
         self.setRowCount(self._count_row)
+        self.setHorizontalHeaderLabels(self._headers)
         self.clear_table()
 
     def __create_cell(self, text):
@@ -23,10 +24,20 @@ class TableModel(QTableWidget):
                 self.setItem(i, j, self.__create_cell(''))
 
     def update_table(self):
-        pass
+        self.clear_table()
+        count = 0
+        for model in self._models:
+            self.setItem(count, 0, self.__create_cell(model.name))
+            self.setItem(count, 1, self.__create_cell(model.type))
+            self.setItem(count, 2, self.__create_cell(model.path))
+            count += 1
+        self.resizeColumnsToContents()
 
     def add_model(self, model):
-        pass
+        self._models.append(model)
+        self.setItem(len(self._models), 0, self.__create_cell(model.name))
+        self.setItem(len(self._models), 1, self.__create_cell(model.type))
+        self.setItem(len(self._models), 2, self.__create_cell(model.path))
 
     def remove_model(self, number):
         self._model.pop(number - 1)
@@ -37,12 +48,12 @@ class TableData(QTableWidget):
     def __init__(self):
         super().__init__()
         self._data = []
-        self._headers = ['Название датасета', 'Абсолютный путь']
         self._count_col = 2
         self._count_row = 20
-        self.setHorizontalHeaderLabels(self._headers)
+        self._headers = ['Название датасета', 'Абсолютный путь']
         self.setColumnCount(self._count_col)
         self.setRowCount(self._count_row)
+        self.setHorizontalHeaderLabels(self._headers)
         self.clear_table()
 
     def __create_cell(self, text):
@@ -55,10 +66,18 @@ class TableData(QTableWidget):
                 self.setItem(i, j, self.__create_cell(''))
 
     def update_table(self):
-        pass
+        self.clear_table()
+        count = 0
+        for data in self._data:
+            self.setItem(count, 0, self.__create_cell(data.name))
+            self.setItem(count, 1, self.__create_cell(data.path))
+            count += 1
+        self.resizeColumnsToContents()
 
     def add_data(self, data):
-        pass
+        self._data.append(data)
+        self.setItem(len(self._data), 0, self.__create_cell(data.name))
+        self.setItem(len(self._data), 1, self.__create_cell(data.path))
 
     def remove_data(self, number):
         self._data.pop(number - 1)
