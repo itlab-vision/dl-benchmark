a7263b8d96c69ed8f157b99e6096ee754ad6ce87
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-05-07 19:25:18 +0300

    delete useless methods, rename methods update and clear in tables

--- gui/view/tables.py
+++ gui/view/tables.py
@@ -14,7 +14,7 @@ class TableModel(QTableWidget):
         self.setRowCount(self._count_row)
         self.setHorizontalHeaderLabels(self._headers)
         self.__resize_columns()
-        self.clear_table()
+        self.clear()
 
     def __resize_columns(self):
         header = self.horizontalHeader()
@@ -26,34 +26,16 @@ class TableModel(QTableWidget):
         cell.setFlags(QtCore.Qt.ItemIsEnabled)
         return cell
 
-    def clear_table(self):
+    def clear(self):
         for i in range(self._count_row):
             for j in range(self._count_col):
                 self.setItem(i, j, self.__create_cell(''))
 
     def update_table(self):
-        self.clear_table()
+        self.clear()
         count = 0
-        for model in self._models:
-            self.setItem(count, 0, self.__create_cell(model.name))
-            self.setItem(count, 1, self.__create_cell(model.type))
-            self.setItem(count, 2, self.__create_cell(model.path))
-            self.setItem(count, 3, self.__create_cell(model.path))
-            self.setItem(count, 4, self.__create_cell(model.path))
-            count += 1
+        # заполнение
         self.resizeColumnsToContents()
-
-    def add_model(self, model):
-        self._models.append(model)
-        self.setItem(len(self._models), 0, self.__create_cell(model.name))
-        self.setItem(len(self._models), 1, self.__create_cell(model.type))
-        self.setItem(len(self._models), 2, self.__create_cell(model.path))
-        self.setItem(len(self._models), 3, self.__create_cell(model.path))
-        self.setItem(len(self._models), 4, self.__create_cell(model.path))
-
-    def remove_model(self, number):
-        self._model.pop(number - 1)
-        self.update_table()
 
 
 class TableData(QTableWidget):
@@ -67,7 +49,7 @@ class TableData(QTableWidget):
         self.setRowCount(self._count_row)
         self.setHorizontalHeaderLabels(self._headers)
         self.__resize_columns()
-        self.clear_table()
+        self.clear()
 
     def __resize_columns(self):
         header = self.horizontalHeader()
@@ -84,23 +66,14 @@ class TableData(QTableWidget):
             for j in range(self._count_col):
                 self.setItem(i, j, self.__create_cell(''))
 
-    def update_table(self):
-        self.clear_table()
+    def update(self):
+        self.clear()
         count = 0
         for data in self._data:
             self.setItem(count, 0, self.__create_cell(data.name))
             self.setItem(count, 1, self.__create_cell(data.path))
             count += 1
         self.resizeColumnsToContents()
-
-    def add_data(self, data):
-        self._data.append(data)
-        self.setItem(len(self._data), 0, self.__create_cell(data.name))
-        self.setItem(len(self._data), 1, self.__create_cell(data.path))
-
-    def remove_data(self, number):
-        self._data.pop(number - 1)
-        self.update_table()
 
 
 class TableTestConfig(QTableWidget):
@@ -115,7 +88,7 @@ class TableTestConfig(QTableWidget):
         self.setRowCount(self._count_row)
         self.setHorizontalHeaderLabels(self._headers)
         self.__resize_columns()
-        self.clear_table()
+        self.clear()
 
     def __resize_columns(self):
         header = self.horizontalHeader()
@@ -127,24 +100,16 @@ class TableTestConfig(QTableWidget):
         cell.setFlags(QtCore.Qt.ItemIsEnabled)
         return cell
 
-    def clear_table(self):
+    def clear(self):
         for i in range(self._count_row):
             for j in range(self._count_col):
                 self.setItem(i, j, self.__create_cell(''))
 
-    def update_table(self):
-        self.clear_table()
+    def update(self):
+        self.clear()
         count = 0
         # заполнение
         self.resizeColumnsToContents()
-
-    def add_test(self, test):
-        self._test.append(test)
-        # поэлементно
-
-    def remove_test(self, number):
-        self._test.pop(number - 1)
-        self.update_table()
 
 
 class TableRemoteConfig(QTableWidget):
@@ -158,7 +123,7 @@ class TableRemoteConfig(QTableWidget):
         self.setRowCount(self._count_row)
         self.setHorizontalHeaderLabels(self._headers)
         self.__resize_columns()
-        self.clear_table()
+        self.clear()
 
     def __resize_columns(self):
         header = self.horizontalHeader()
@@ -170,24 +135,16 @@ class TableRemoteConfig(QTableWidget):
         cell.setFlags(QtCore.Qt.ItemIsEnabled)
         return cell
 
-    def clear_table(self):
+    def clear(self):
         for i in range(self._count_row):
             for j in range(self._count_col):
                 self.setItem(i, j, self.__create_cell(''))
 
-    def update_table(self):
-        self.clear_table()
+    def update(self):
+        self.clear()
         count = 0
         # заполнение
         self.resizeColumnsToContents()
-
-    def add_test(self, parameter):
-        self._parameters.append(parameter)
-        # поэлементно
-
-    def remove_test(self, number):
-        self._parameters.pop(number - 1)
-        self.update_table()
 
 
 class TableDeployConfig(QTableWidget):
