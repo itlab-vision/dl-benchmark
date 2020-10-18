6d3fe2f2a461d54ad1b891dcdf6c4995e03f4d01
KchnKchn <khvatov.alexander@gmail.com>
2020-10-18 18:47:26 +0300

    deployment flake8 fix

--- src/deployment/windows_remote_helper.py
+++ src/deployment/windows_remote_helper.py
@@ -1,37 +1,39 @@
-import wmi # pylint: disable=E0401
+import wmi  # pylint: disable=E0401
 from remote_helper import remote_helper
+
 
 class windows_remote_hepler(remote_helper):
     def __init__(self, log):
         super().__init__(log)
 
-
     def connect(self, machine_ip, login, password):
-        new_connection = wmi.WMI(machine_ip, user = login,
-            password = password)
-
+        new_connection = wmi.WMI(
+            machine_ip,
+            user=login,
+            password=password
+        )
         return new_connection
-
 
     def execute(self, con, command):
         process_startup = con.Win32_ProcessStartup.new()
-        process_id, result = con.Win32_Process.Create(CommandLine = command,
-            ProcessStartupInformation = process_startup)
-
+        process_id, result = con.Win32_Process.Create(
+            CommandLine=command,
+            ProcessStartupInformation=process_startup
+        )
         watcher = None
         if result == 0:
             self.my_log.info('Process started successfully {}'.format(process_id))
-            con.watch_for(notification_type = 'Deletion',
-                wmi_class = 'Win32_Process', ProcessId = process_id)
+            con.watch_for(
+                notification_type='Deletion',
+                wmi_class='Win32_Process',
+                ProcessId=process_id
+            )
         else:
             self.my_log.info('Problem creating process {}'.format(result))
-
         return watcher
-
 
     def execute_python(self, con, command):
         return self.execute(con, 'python {}'.format(command))
-
 
     def wait(self, process):
         process_status = process()
