e63b9b11daf3552cbd86d0563f723ff76c1a46b7
Alexandra <alexa.sanechka@ya.ru>
2021-03-27 16:39:44 +0300

    Fixed test results

--- src/accuracy_checker/config/datasets.py
+++ src/accuracy_checker/config/datasets.py
@@ -4,9 +4,7 @@ from .metrics import metric  # pylint: disable=E0402
 class dataset:
     def __init__(self, data):
         DATATSET_NAME_TAG = 'name'
-        SIZE_TAG = 'subsample_size'
         METRICS_TAG = 'metrics'
 
         self.dataset_name = data[DATATSET_NAME_TAG]
-        self.size = data[SIZE_TAG]
         self.metrics = [metric.get_metric(metric_data) for metric_data in data[METRICS_TAG]]
