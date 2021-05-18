dd9e0d7bee99508df1b872d86cd95fb0b9b89d4f
Alexandra Sidorova <alexa.sanechka@ya.ru>
2021-05-18 21:42:24 +0300

    [Accuracy Checker] Fixed docker and added some changes

--- src/accuracy_checker/process.py
+++ src/accuracy_checker/process.py
@@ -2,21 +2,26 @@ from result import result
 
 
 class process:
-    def __init__(self, log, executor, parameters):
+    def __init__(self, log, executor, test):
         self.__log = log
         self.__executor = executor
-        self.__parameters = parameters
+        self.__test = test
         self.__output = None
+        self.__supported_frameworks = {'OpenVINO DLDT': 'dlsdk', 'Caffe': 'caffe', 'TensorFlow': 'tf'}
 
     def __fill_command_line(self):
-        command_line = 'accuracy_check -c {0} -m {1} -s {2}'.format(self.__parameters.config, self.__parameters.models,
-                                                                    self.__parameters.source)
-        if self.__parameters.annotations:
-            self.__add_annotations_for_cmd_line(command_line, self.__parameters.annotations)
-        if self.__parameters.definitions:
-            self.__add_definitions_for_cmd_line(command_line, self.__parameters.definitions)
-        if self.__parameters.extensions:
-            self.__add_extensions_for_cmd_line(command_line, self.__parameters.extensions)
+        command_line = 'accuracy_check -c {0} -m {1} -s {2} -td {3}'.format(self.__test.path_to_config,
+                                                                            self.__test.model.directory,
+                                                                            self.__test.parameters.source,
+                                                                            self.__test.device
+                                                                            )
+        command_line = self.__add_framework_for_cmd_line(command_line, self.__supported_frameworks[self.__test.framework])
+        if self.__test.parameters.annotations:
+            command_line = self.__add_annotations_for_cmd_line(command_line, self.__test.parameters.annotations)
+        if self.__test.parameters.definitions:
+            command_line = self.__add_definitions_for_cmd_line(command_line, self.__test.parameters.definitions)
+        if self.__test.parameters.extensions:
+            command_line = self.__add_extensions_for_cmd_line(command_line, self.__test.parameters.extensions)
         return command_line
 
     @staticmethod
@@ -31,15 +36,21 @@ class process:
     def __add_extensions_for_cmd_line(command_line, extensions):
         return '{0} -e {1}'.format(command_line, extensions)
 
-    def execute(self, target_framework):
+    @staticmethod
+    def __add_framework_for_cmd_line(command_line, framework):
+        return '{0} -tf {1}'.format(command_line, framework)
+
+    def execute(self, idx):
         command_line = self.__fill_command_line()
         if command_line == '':
             self.__log.error('Command line is empty')
-        self.__log.info('Start accuracy check for config: {}'.format(self.__parameters.config))
-        self.__executor.set_target_framework(target_framework)
+        self.__log.info('Start accuracy check for {0} test: {1}'.format(idx, self.__test.model.name))
+        self.__executor.set_target_framework(self.__test.framework)
+        command_line = self.__executor.prepare_command_line(self.__test, command_line)
         self.__output = self.__executor.execute_process(command_line)
         if type(self.__output) is not list:
             self.__output = self.__output.decode("utf-8").split('\n')
+        print(self.__output)
 
     def get_result_parameters(self):
         return result.parser_test_results(self.__output)
--- src/accuracy_checker/result.py
+++ src/accuracy_checker/result.py
@@ -1,12 +1,10 @@
 class result:
-    def __init__(self, status, model, launcher, device, dataset, precision, objects, accuracy):
+    def __init__(self, status, model, launcher, device, dataset, accuracy):
         self.__status = status
         self.__model = model
         self.__launcher = launcher
         self.__device = device
         self.__dataset = dataset
-        self.__precision = precision
-        self.__objects = objects
         self.__accuracy = accuracy
 
     def is_failed(self):
@@ -14,8 +12,7 @@ class result:
 
     def get_result_dict(self):
         return {'status': self.__status, 'model': self.__model, 'launcher': self.__launcher, 'device': self.__device,
-                'dataset': self.__dataset, 'precision': self.__precision, 'objects': self.__objects,
-                'accuracy': self.__accuracy}
+                'dataset': self.__dataset, 'accuracy': self.__accuracy}
 
     @staticmethod
     def has_error(strings):
@@ -32,7 +29,6 @@ class result:
         TAG_LAUNCHER = 'launcher:'
         TAG_DEVICE = 'device:'
         TAG_DATASET = 'dataset:'
-        TAG_PRECISION = 'precision:'
         TAG_OBJECTS = 'objects'
 
         res = [str.replace(' ', '') for str in res]
@@ -42,44 +38,39 @@ class result:
 
         error = result.has_error(res)
         status = 'FAILED' if error else 'SUCCESS'
-        models = [value[len(TAG_MODEL):] for value in res if TAG_MODEL in value]
+        models = [value[len(TAG_MODEL):].replace('\r', '') for value in res if TAG_MODEL in value]
         if not models:
             raise ValueError('Information about model was not found in test result', idx)
-        launchers = [value[len(TAG_LAUNCHER):] for value in res if TAG_LAUNCHER in value]
+        launchers = [value[len(TAG_LAUNCHER):].replace('\r', '') for value in res if TAG_LAUNCHER in value]
         if not launchers:
             raise ValueError('Information about launcher was not found in test result', idx)
-        devices = [value[len(TAG_DEVICE):] for value in res if TAG_DEVICE in value]
+        devices = [value[len(TAG_DEVICE):].replace('\r', '') for value in res if TAG_DEVICE in value]
         if not devices:
             raise ValueError('Information about device was not found in test result', idx)
-        datasets = [value[len(TAG_DATASET):] for value in res if TAG_DATASET in value]
+        datasets = [value[len(TAG_DATASET):].replace('\r', '') for value in res if TAG_DATASET in value]
         if not datasets:
             raise ValueError('Information about dataset was not found in test result', idx)
-        precisions = [value[len(TAG_PRECISION):] for value in res if TAG_PRECISION in value] if not error else ['']
-        if not precisions:
-            raise ValueError('Information about precision was not found in test result', idx)
-        objects = [value[:value.find(TAG_OBJECTS)] for value in res if TAG_OBJECTS in value] if not error else ['']
-        if not objects:
-            raise ValueError('Information about objects was not found in test result', idx)
         if not error:
             idx = [res.index(value) for value in res if TAG_OBJECTS in value]
-            accuracies = [res[i][res[i].index(':') + 1:] for i in range(idx[0] + 1, len(res))]
+            accuracies = [res[i][res[i].index(':') + 1:].replace('\r', '') for i in range(idx[0] + 1, len(res))]
         else:
             accuracies = ['']
         if not accuracies:
             raise ValueError('Information about accuracy was not found in test result', idx)
 
-        return [result(status, models[0], launchers[0], devices[0], datasets[0], precisions[0], objects[0], accuracy)
-                for accuracy in accuracies]
+        return [result(status=status, model=models[0], launcher=launchers[0], device=devices[0],
+                       dataset=datasets[0], accuracy=accuracy) for accuracy in accuracies]
 
     @staticmethod
     def parser_test_results(out):
         results = []
-        result_start = 'Processing info:'
-        count = out.count(result_start)
-        begin = out.index(result_start)
+        RESULT_START_TAG = 'Processing info:'
+        results_idx = [out.index(value) for value in out if RESULT_START_TAG in value]
+        count = len(results_idx)
+        begin = results_idx[0]
         for idx in range(count):
             if idx != (count - 1):
-                end = out.index(result_start, begin + 1) + 1
+                end = results_idx[idx + 1]
                 results.append(result.parser_test_result(out[begin:end], idx))
                 begin = end
             else:
