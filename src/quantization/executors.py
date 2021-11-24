0425ad674c9aa48f3cfda232b58443890d6ba81d
AlibekovMurad5202 <MuradAlibekov2000@gmail.com>
2021-11-24 17:51:20 +0300

    fixed docker executor

--- src/quantization/executors.py
+++ src/quantization/executors.py
@@ -13,8 +13,8 @@ class executor(metaclass=abc.ABCMeta):
     def get_executor(executor_type, log):
         if executor_type == 'host_machine':
             return host_executor(log)
-        elif executor_type == 'docker_container':
-            return docker_executor(log)
+        # elif executor_type == 'docker_container':
+        #     return docker_executor(log)
         else:
             raise ValueError('Wrong executor type!')
 
@@ -48,19 +48,8 @@ class docker_executor(executor):
     def __init__(self, log):
         super().__init__(log)
         client = docker.from_env()
-        # self.my_containers = {cont for cont in client.containers.list()}
         self.my_container_dict = {cont.name: cont for cont in client.containers.list()}
 
 
     def execute_process(self, command_line):
-        command_line = 'bash -c "source /root/.bashrc && {}"'.format(command_line)
-        out = []
-
-        # for cont in self.my_containers:
-        #     out.append(cont.exec_run(command_line, tty=True, privileged=True))
-        out.append(self.my_containers['OpenVINO_DLDT'].exec_run(
-            command_line,
-            tty=True,
-            privileged=True
-        ))
-        return out
+        pass
