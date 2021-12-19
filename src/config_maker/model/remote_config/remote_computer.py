3179421fb78e0b743a34718d442f360dc11c039d
Alexandra Sidorova <alexa.sanechka@ya.ru>
2021-12-19 13:44:56 +0300

    updated remote config

--- src/config_maker/model/remote_config/remote_computer.py
+++ src/config_maker/model/remote_config/remote_computer.py
@@ -1,11 +1,147 @@
+import abc
+# pylint: disable-next=E0401
+from tags import CONFIG_COMPUTER_TAG, CONFIG_IP_TAG, CONFIG_LOGIN_TAG, CONFIG_PASSWORD_TAG, CONFIG_OS_TAG, \
+    CONFIG_BENCHMARK_TAG, CONFIG_ACCURACY_CHECKER_TAG, CONFIG_FTP_CLIENT_PATH_TAG, CONFIG_CONFIG_TAG, \
+    CONFIG_EXECUTOR_TAG, CONFIG_LOG_FILE_TAG, CONFIG_RESULT_FILE_TAG, CONFIG_AC_DATASET_PATH_TAG, CONFIG_DEFINITION_PATH
+
+
 class RemoteComputer:
-    def __init__(self, ip=None, login=None, password=None, os=None, path_to_ftp_client=None, benchmark_config=None,
-                 log_file=None, res_file=None):
+    def __init__(self, ip, login, password, os, path_to_ftp_client, *args):
         self.ip = ip
         self.login = login
         self.password = password
         self.os = os
         self.path_to_ftp_client = path_to_ftp_client
-        self.benchmark_config = benchmark_config
-        self.log_file = log_file
-        self.res_file = res_file
+        self.benchmark = BenchmarkComponent(*args[0:4]) if len(args) > 2 else args[0]
+        self.accuracy_checker = AccuracyCheckerComponent(*args[4:-1]) if len(args) > 2 else args[1]
+
+    def create_dom(self, file):
+        DOM_COMPUTER_TAG = file.createElement(CONFIG_COMPUTER_TAG)
+        DOM_IP_TAG = file.createElement(CONFIG_IP_TAG)
+        DOM_LOGIN_TAG = file.createElement(CONFIG_LOGIN_TAG)
+        DOM_PASSWORD_TAG = file.createElement(CONFIG_PASSWORD_TAG)
+        DOM_OS_TAG = file.createElement(CONFIG_OS_TAG)
+        DOM_FTP_CLIENT_PATH_TAG = file.createElement(CONFIG_FTP_CLIENT_PATH_TAG)
+        DOM_BENCHMARK_TAG = self.benchmark.create_dom(file)
+        DOM_ACCURACY_CHECKER_TAG = self.accuracy_checker.create_dom(file)
+
+        DOM_IP_TAG.appendChild(file.createTextNode(self.ip))
+        DOM_LOGIN_TAG.appendChild(file.createTextNode(self.login))
+        DOM_PASSWORD_TAG.appendChild(file.createTextNode(self.password))
+        DOM_OS_TAG.appendChild(file.createTextNode(self.os))
+        DOM_FTP_CLIENT_PATH_TAG.appendChild(file.createTextNode(self.path_to_ftp_client))
+        DOM_COMPUTER_TAG.appendChild(DOM_IP_TAG)
+        DOM_COMPUTER_TAG.appendChild(DOM_LOGIN_TAG)
+        DOM_COMPUTER_TAG.appendChild(DOM_PASSWORD_TAG)
+        DOM_COMPUTER_TAG.appendChild(DOM_OS_TAG)
+        DOM_COMPUTER_TAG.appendChild(DOM_BENCHMARK_TAG)
+        DOM_COMPUTER_TAG.appendChild(DOM_ACCURACY_CHECKER_TAG)
+
+        return DOM_COMPUTER_TAG
+
+    @staticmethod
+    def parse(dom):
+        parsed_computers = dom.getElementsByTagName(CONFIG_COMPUTER_TAG)
+        computers = []
+        for parsed_computer in parsed_computers:
+            ip = parsed_computer.getElementsByTagName(CONFIG_IP_TAG)[0].firstChild.data
+            login = parsed_computer.getElementsByTagName(CONFIG_LOGIN_TAG)[0].firstChild.data
+            password = parsed_computer.getElementsByTagName(CONFIG_PASSWORD_TAG)[0].firstChild.data
+            os = parsed_computer.getElementsByTagName(CONFIG_OS_TAG)[0].firstChild.data
+            ftp_client_path = parsed_computer.getElementsByTagName(CONFIG_FTP_CLIENT_PATH_TAG)[0].firstChild.data
+            benchmark = BenchmarkComponent.parse(parsed_computer.getElementsByTagName(CONFIG_BENCHMARK_TAG)[0])
+            accuracy_checker = AccuracyCheckerComponent.parse(
+                parsed_computer.getElementsByTagName(CONFIG_ACCURACY_CHECKER_TAG)[0])
+            computers.append(RemoteComputer(ip, login, password, os, ftp_client_path, benchmark, accuracy_checker))
+        return computers
+
+
+class Component(metaclass=abc.ABCMeta):
+    def __init__(self, config, executor, log_file, res_file):
+        self.parameters = {
+            CONFIG_CONFIG_TAG: config,
+            CONFIG_EXECUTOR_TAG: executor,
+            CONFIG_LOG_FILE_TAG: log_file,
+            CONFIG_RESULT_FILE_TAG: res_file
+        }
+        self.CONFIG_ROOT_TAG = None
+
+    @staticmethod
+    @abc.abstractmethod
+    def parse(dom):
+        pass
+
+    def create_dom(self, file):
+        DOM_ROOT_TAG = file.createElement(self.CONFIG_ROOT_TAG)
+
+        for key in self.parameters:
+            DOM_PARAMETER = file.createElement(key)
+            DOM_PARAMETER.appendChild(file.createTextNode(self.parameters[key]))
+            DOM_ROOT_TAG.appendChild(DOM_PARAMETER)
+
+        return DOM_ROOT_TAG
+
+
+class BenchmarkComponent(Component):
+    def __init__(self, config, executor, log_file, res_file):
+        super().__init__(config, executor, log_file, res_file)
+        self.CONFIG_ROOT_TAG = CONFIG_BENCHMARK_TAG
+
+    @staticmethod
+    def parse(dom):
+        config = ''
+        executor = ''
+        log_file = ''
+        res_file = ''
+
+        config_node = dom.getElementsByTagName(CONFIG_CONFIG_TAG)[0].firstChild
+        if config_node:
+            config = config_node.data
+        executor_node = dom.getElementsByTagName(CONFIG_EXECUTOR_TAG)[0].firstChild
+        if executor_node:
+            executor = executor_node.data
+        log_file_node = dom.getElementsByTagName(CONFIG_LOG_FILE_TAG)[0].firstChild
+        if log_file_node:
+            log_file = log_file_node.data
+        res_file_node = dom.getElementsByTagName(CONFIG_RESULT_FILE_TAG)[0].firstChild
+        if res_file_node:
+            res_file = res_file_node.data
+        return BenchmarkComponent(config, executor, log_file, res_file)
+
+
+class AccuracyCheckerComponent(Component):
+    def __init__(self, config, executor, dataset_path, definition_path, log_file, res_file):
+        super().__init__(config, executor, log_file, res_file)
+        self.parameters[CONFIG_AC_DATASET_PATH_TAG] = dataset_path
+        self.parameters[CONFIG_DEFINITION_PATH] = definition_path
+        self.CONFIG_ROOT_TAG = CONFIG_ACCURACY_CHECKER_TAG
+
+    @staticmethod
+    def parse(dom):
+        config = ''
+        executor = ''
+        dataset_path = ''
+        definition_path = ''
+        log_file = ''
+        res_file = ''
+
+        config_node = dom.getElementsByTagName(CONFIG_CONFIG_TAG)[0].firstChild
+        if config_node:
+            config = config_node.data
+        executor_node = dom.getElementsByTagName(CONFIG_EXECUTOR_TAG)[0].firstChild
+        if executor_node:
+            executor = executor_node.data
+        dataset_path_node = dom.getElementsByTagName(CONFIG_AC_DATASET_PATH_TAG)[0].firstChild
+        if dataset_path_node:
+            dataset_path = dataset_path_node.data
+        definition_path_node = dom.getElementsByTagName(CONFIG_DEFINITION_PATH)[0].firstChild
+        if definition_path_node:
+            definition_path = definition_path_node.data
+        log_file_node = dom.getElementsByTagName(CONFIG_LOG_FILE_TAG)[0].firstChild
+        if log_file_node:
+            log_file = log_file_node.data
+        res_file_node = dom.getElementsByTagName(CONFIG_RESULT_FILE_TAG)[0].firstChild
+        if res_file_node:
+            res_file = res_file_node.data
+
+        return AccuracyCheckerComponent(config, executor, dataset_path, definition_path, log_file, res_file)
--- src/config_maker/model/remote_config/remote_config.py
+++ src/config_maker/model/remote_config/remote_config.py
@@ -1,6 +1,7 @@
 import os
 from xml.dom import minidom
 from .remote_computer import RemoteComputer  # pylint: disable=E0402
+from tags import CONFIG_COMPUTERS_TAG  # pylint: disable=E0401
 
 
 class RemoteConfig:
@@ -10,13 +11,11 @@ class RemoteConfig:
     def get_computers(self):
         return self.__computers
 
-    def add_computer(self, ip, login, password, os, path_to_ftp_client, benchmark_config, log_file, res_file):
-        self.__computers.append(RemoteComputer(ip, login, password, os, path_to_ftp_client, benchmark_config,
-                                               log_file, res_file))
+    def add_computer(self, ip, login, password, os, path_to_ftp_client, *args):
+        self.__computers.append(RemoteComputer(ip, login, password, os, path_to_ftp_client, *args))
 
-    def change_computer(self, row, ip, login, password, os, path_to_ftp_client, benchmark_config, log_file, res_file):
-        self.__computers[row] = RemoteComputer(ip, login, password, os, path_to_ftp_client, benchmark_config,
-                                               log_file, res_file)
+    def change_computer(self, row, ip, login, password, os, path_to_ftp_client, *args):
+        self.__computers[row] = RemoteComputer(ip, login, password, os, path_to_ftp_client, *args)
 
     def delete_computer(self, index):
         self.__computers.pop(index)
@@ -35,72 +34,17 @@ class RemoteConfig:
         self.__computers.clear()
 
     def parse_config(self, path_to_config):
-        CONFIG_ROOT_TAG = 'Computer'
-        CONFIG_IP_TAG = 'IP'
-        CONFIG_LOGIN_TAG = 'Login'
-        CONFIG_PASSWORD_TAG = 'Password'
-        CONFIG_OS_TAG = 'OS'
-        CONFIG_FTP_CLIENT_PATH_TAG = 'FTPClientPath'
-        CONFIG_BENCHMARK_CONFIG_TAG = 'BenchmarkConfig'
-        CONFIG_LOG_FILE_TAG = 'LogFile'
-        CONFIG_RESULT_FILE_TAG = 'ResultFile'
-
         parsed_config = minidom.parse(path_to_config)
-        computers = parsed_config.getElementsByTagName(CONFIG_ROOT_TAG)
-        self.__computers.clear()
-        for idx, computer in enumerate(computers):
-            self.__computers.append(RemoteComputer())
-            self.__computers[idx].ip = computer.getElementsByTagName(
-                CONFIG_IP_TAG)[0].firstChild.data
-            self.__computers[idx].login = computer.getElementsByTagName(
-                CONFIG_LOGIN_TAG)[0].firstChild.data
-            self.__computers[idx].password = computer.getElementsByTagName(
-                CONFIG_PASSWORD_TAG)[0].firstChild.data
-            self.__computers[idx].os = computer.getElementsByTagName(
-                CONFIG_OS_TAG)[0].firstChild.data
-            self.__computers[idx].path_to_ftp_client = computer.getElementsByTagName(
-                CONFIG_FTP_CLIENT_PATH_TAG)[0].firstChild.data
-            self.__computers[idx].benchmark_config = computer.getElementsByTagName(
-                CONFIG_BENCHMARK_CONFIG_TAG)[0].firstChild.data
-            self.__computers[idx].log_file = computer.getElementsByTagName(
-                CONFIG_LOG_FILE_TAG)[0].firstChild.data
-            self.__computers[idx].res_file = computer.getElementsByTagName(
-                CONFIG_RESULT_FILE_TAG)[0].firstChild.data
+        self.__computers = RemoteComputer.parse(parsed_config)
 
     def create_config(self, path_to_config):
         if len(self.__computers) == 0:
             return False
         file = minidom.Document()
-        CONFIG_ROOT_TAG = file.createElement('Computers')
-        file.appendChild(CONFIG_ROOT_TAG)
-        for computer in self.__computers:
-            CONFIG_COMPUTER_TAG = file.createElement('Computer')
-            CONFIG_IP_TAG = file.createElement('IP')
-            CONFIG_LOGIN_TAG = file.createElement('Login')
-            CONFIG_PASSWORD_TAG = file.createElement('Password')
-            CONFIG_OS_TAG = file.createElement('OS')
-            CONFIG_FTP_CLIENT_PATH_TAG = file.createElement('FTPClientPath')
-            CONFIG_BENCHMARK_CONFIG_TAG = file.createElement('BenchmarkConfig')
-            CONFIG_LOG_FILE_TAG = file.createElement('LogFile')
-            CONFIG_RESULT_FILE_TAG = file.createElement('ResultFile')
-
-            CONFIG_IP_TAG.appendChild(file.createTextNode(computer.ip))
-            CONFIG_LOGIN_TAG.appendChild(file.createTextNode(computer.login))
-            CONFIG_PASSWORD_TAG.appendChild(file.createTextNode(computer.password))
-            CONFIG_OS_TAG.appendChild(file.createTextNode(computer.os))
-            CONFIG_FTP_CLIENT_PATH_TAG.appendChild(file.createTextNode(computer.path_to_ftp_client))
-            CONFIG_BENCHMARK_CONFIG_TAG.appendChild(file.createTextNode(computer.benchmark_config))
-            CONFIG_LOG_FILE_TAG.appendChild(file.createTextNode(computer.log_file))
-            CONFIG_RESULT_FILE_TAG.appendChild(file.createTextNode(computer.res_file))
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_IP_TAG)
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_LOGIN_TAG)
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_PASSWORD_TAG)
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_OS_TAG)
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_FTP_CLIENT_PATH_TAG)
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_BENCHMARK_CONFIG_TAG)
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_LOG_FILE_TAG)
-            CONFIG_COMPUTER_TAG.appendChild(CONFIG_RESULT_FILE_TAG)
-            CONFIG_ROOT_TAG.appendChild(CONFIG_COMPUTER_TAG)
+        DOM_ROOT_TAG = file.createElement(CONFIG_COMPUTERS_TAG)
+        file.appendChild(DOM_ROOT_TAG)
+        for model in self.__computers:
+            DOM_ROOT_TAG.appendChild(model.create_dom(file))
         xml_str = file.toprettyxml(indent="\t", encoding="utf-8")
         with open(path_to_config, 'wb') as f:
             f.write(xml_str)
