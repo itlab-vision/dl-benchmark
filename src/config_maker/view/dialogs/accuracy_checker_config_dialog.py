c139fee2a54e39f619fcd8670e2932c5228768cb
Alexandra Sidorova <alexa.sanechka@ya.ru>
2022-01-29 12:41:38 +0300

    removed unused import abc

--- src/config_maker/view/dialogs/accuracy_checker_config_dialog.py
+++ src/config_maker/view/dialogs/accuracy_checker_config_dialog.py
@@ -1,4 +1,3 @@
-import abc
 from PyQt5.QtWidgets import QDialog, QLabel, QComboBox, QLineEdit, QPushButton, QGridLayout, QMessageBox
 # pylint: disable-next=E0401
 from tags import CONFIG_MODEL_TAG, CONFIG_PARAMETERS_TAG, CONFIG_FRAMEWORK_TAG, CONFIG_DEVICE_TAG, CONFIG_CONFIG_TAG
