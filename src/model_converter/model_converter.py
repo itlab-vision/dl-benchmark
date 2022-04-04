c088610e3bca560397352b1a17c09b19390985ed
Evgenii Vasiliev <eugene.unn@gmail.com>
2022-04-04 04:24:26 +0300

    Add batch_converter

---
+++ src/model_converter/model_converter.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import argparse
+import logging as log
+from process import process
+from shape_parser import get_new_input_shape_by_model_name
+from utils import get_all_downloaded_public_models_in_dir
+
+import traceback
+
+
+def build_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-b', '--batch_sizes', 
+        help='Batches to convert models',
+        type = int, nargs='+', default = [8]
+    )
+    parser.add_argument(
+        '-d', '--dir',
+        help = 'Root directory where to store converter models ',
+        type = str, default = './'
+    )
+    parser.add_argument(
+        '-z', '--zoo_config_dir',
+        help = 'Directory where public models config plased',
+        default = ''
+    )
+    config = parser.parse_args()
+    return config
+
+
+def model_conversion(params, log):
+    models_list = get_all_downloaded_public_models_in_dir(params.dir)
+    log.info(f'List of models to convert {models_list}')
+    for model in models_list:
+        log.info(f'Start conversion model {model}')
+        for batch in params.batch_sizes:
+            convert_process = process(model, batch, params, log)
+            convert_process.execute()
+        log.info(f'End conversion model {model}!')
+
+
+def main():
+    log.basicConfig(
+        format='[ %(levelname)s][%(filename)s %(lineno)s] %(message)s',
+        level=log.INFO,
+        stream=sys.stdout
+    )
+    conversion_parameters = build_argparser()
+
+    try:   
+        log.info('Start conversion on')
+        model_conversion(conversion_parameters, log)
+        log.info('End conversion!')
+    except Exception as exp:
+        log.error(str(exp))
+        traceback.print_exc()
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
---
+++ src/model_converter/process.py
@@ -0,0 +1,68 @@
+import os
+from subprocess import Popen, PIPE, STDOUT
+from shape_parser import get_new_input_shape_by_model_name
+
+
+class process:
+    def __init__(self, model, batch, parameters, log):
+        self.__model  = model
+        self.__batch = batch
+        self.__my_log = log
+        self.__my_parameters = parameters
+        self.__my_output = None
+
+    @staticmethod
+    def __add_output_dir_for_cmd_line(command_line, output_dir, batch):
+        if batch == 1:
+            new_path_with_batch = output_dir
+        else: 
+            new_path_with_batch = os.path.join(output_dir, str(batch))
+        return '{0} --output_dir "{1}"'.format(command_line, new_path_with_batch)
+
+    @staticmethod
+    def __add_shape_for_cmd_line(command_line, zoo_dir, model_name, batch):
+        shape_line = get_new_input_shape_by_model_name(zoo_dir, model_name, batch)
+        return '{0}{1}'.format(command_line, shape_line)
+
+    def _fill_command_line(self):
+        cmd_line_base = 'omz_converter --name {0}'.format(self.__model)
+
+        add_mo_params = ''
+        add_mo_params = self.__add_shape_for_cmd_line(
+            add_mo_params,
+            self.__my_parameters.zoo_config_dir,
+            self.__model,
+            self.__batch
+        )
+
+        common_params = ''
+        if self.__my_parameters.dir:
+            common_params = self.__add_output_dir_for_cmd_line(
+                common_params,
+                self.__my_parameters.dir,
+                self.__batch
+            )
+
+        add_mo_params = f'--add_mo_arg="{add_mo_params}"'
+        command_line = '{0} {1} {2}'.format(cmd_line_base, add_mo_params, common_params)
+        return command_line
+
+
+    def execute(self):
+        command_line = self._fill_command_line()
+        if command_line == '':
+            self.__my_log.error('Command line is empty')
+        self.__my_log.info(f'Start converting with command line: {command_line}')
+
+        process = Popen(
+            command_line,
+            env=os.environ.copy(),
+            shell=True,
+            stdout=PIPE,
+            stderr=STDOUT,
+            universal_newlines=True
+        )
+        out, _ = process.communicate()
+        self.__my_log.info(f'{out}')
+        #if type(out) is not list:
+        #    self.__my_log.info(self.__my_output)
