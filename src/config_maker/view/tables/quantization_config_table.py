13d3bf4d2eca7b7b5aeca8f7408d4d535c2189ed
AlibekovMurad5202 <MuradAlibekov2000@gmail.com>
2022-04-23 23:33:43 +0300

    fixed E1120 (pylint)

--- src/config_maker/view/tables/quantization_config_table.py
+++ src/config_maker/view/tables/quantization_config_table.py
@@ -16,7 +16,7 @@ class QuantizationConfigTable(Table):
         self.setHorizontalHeaderLabels(self.__headers)
         self._resize_columns()
         self.clear()
-        self.clicked.connect(self.clicked_table)
+        self.clicked().connect(self.clicked_table)
 
     def update(self, q_models):
         self.clear()
