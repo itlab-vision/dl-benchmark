97a74d071f28977f9e6eb5917e7be7ad72809ffe
Alexandra Sidorova <alexa.sanechka@ya.ru>
2022-04-25 14:57:40 +0300

    Fixed pylint in gui. Second attempt

--- src/config_maker/view/widgets/data_widgets/data_settings_widget.py
+++ src/config_maker/view/widgets/data_widgets/data_settings_widget.py
@@ -27,13 +27,13 @@ class DataSettingsWidget(QWidget):
         self.__set_connections()
 
     def __set_connections(self):
-        self.__buttons.get_buttons()['Add information'].clicked().connect(self.__show_dialog_add_dataset)
-        self.__buttons.get_buttons()['Delete information'].clicked().connect(self.__click_delete_button)
-        self.__buttons.get_buttons()['Change information'].clicked().connect(self.__show_dialog_change_dataset)
-        self.__buttons.get_buttons()['Copy information'].clicked().connect(self.__copy_data)
-        self.__buttons.get_buttons()['Load data'].clicked().connect(self.__show_dialog_parser_config)
-        self.__buttons.get_buttons()['Save data'].clicked().connect(self.__show_dialog_create_config)
-        self.__buttons.get_buttons()['Clear table'].clicked().connect(self.clearSignal.emit)
+        self.__buttons.get_buttons()['Add information'].clicked.connect(self.__show_dialog_add_dataset)
+        self.__buttons.get_buttons()['Delete information'].clicked.connect(self.__click_delete_button)
+        self.__buttons.get_buttons()['Change information'].clicked.connect(self.__show_dialog_change_dataset)
+        self.__buttons.get_buttons()['Copy information'].clicked.connect(self.__copy_data)
+        self.__buttons.get_buttons()['Load data'].clicked.connect(self.__show_dialog_parser_config)
+        self.__buttons.get_buttons()['Save data'].clicked.connect(self.__show_dialog_create_config)
+        self.__buttons.get_buttons()['Clear table'].clicked.connect(self.clearSignal.emit)
 
     def __show_dialog_add_dataset(self):
         dialog = DataDialog(self)
