9370a9bf1e8dba56132f4c646b94ce7656cb6302
valentina-kustikova <valentina.kustikova.gmail.com>
2022-11-04 05:54:55 +0300

    Fix coding style

--- src/csv2xlsx/converter.py
+++ src/csv2xlsx/converter.py
@@ -2,22 +2,32 @@ import sys
 import argparse
 import logging
 from importlib import reload
+
 from benchmark_table_creator import XlsxBenchmarkTable
 from accuracy_table_creator import XlsxAccuracyTable
 
 
-def build_parser():
+def cli_argument_parser():
     logging.info('START: build_parser()')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--tables', type=str,
+
+    parser.add_argument('-t', '--tables',
+                        type=str,
                         help='Paths to the inference tables in csv format.',
-                        nargs='+', required=True)
-    parser.add_argument('-r', '--result_table', type=str,
-                        help='Full name of the resulting file', required=True)
-    parser.add_argument('-k', '--table_kind', type=str, help='Kind of table: ',
+                        nargs='+',
+                        required=True)
+
+    parser.add_argument('-r', '--result_table',
+                        type=str,
+                        help='Full name of the resulting file',
+                        required=True)
+
+    parser.add_argument('-k', '--table_kind',
+                        type=str, help='Kind of table: ',
                         choices=['benchmark', 'accuracy_checker'],
                         default='benchmark')
+
     paths_table_csv = parser.parse_args().tables
     path_table_xlsx = parser.parse_args().result_table
     table_kind = parser.parse_args().table_kind
@@ -34,7 +44,8 @@ def convert_csv_table_to_xlsx(paths_table_csv, path_table_xlsx, table_type):
     elif table_type == 'accuracy_checker':
         table_xlsx = XlsxAccuracyTable(paths_table_csv, path_table_xlsx)
     else:
-        raise ValueError(f'Incorrect value of table type "{table_type}"')
+        raise ValueError(f'Incorrect value of the table type "{table_type}"')
+
     table_xlsx.read_csv_table()
     table_xlsx.create_table_header()
     table_xlsx.create_table_rows()
@@ -50,8 +61,16 @@ def main():
     logging.basicConfig(stream=sys.stdout,
                         format='%(asctime)s %(levelname)s: %(message)s',
                         level=logging.INFO)
-    paths_table_csv, path_table_xlsx, table_type = build_parser()
-    convert_csv_table_to_xlsx(paths_table_csv, path_table_xlsx, table_type)
+
+    paths_table_csv, path_table_xlsx, table_type = cli_argument_parser()
+
+    try:
+        convert_csv_table_to_xlsx(paths_table_csv, path_table_xlsx, table_type)
+    except ValueError as ex: 
+        logging.error(f'{ex}')
+        return 1
+    
+    return 0
 
 
 if __name__ == '__main__':
