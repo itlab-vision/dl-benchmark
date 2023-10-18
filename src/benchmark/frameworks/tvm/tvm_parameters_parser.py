e7692687aae08418c53134610323a045737e16f7
ismukhin <vvstanka39@gmail.com>
2023-10-18 19:04:41 +0300

    template for tvm benchmark

---
+++ src/benchmark/frameworks/tvm/tvm_parameters_parser.py
@@ -0,0 +1,5 @@
+from ..config_parser.dependent_parameters_parser import DependentParametersParser
+from ..config_parser.framework_parameters_parser import FrameworkParameters
+
+class TVMParametersParser(DependentParametersParser):
+    pass
