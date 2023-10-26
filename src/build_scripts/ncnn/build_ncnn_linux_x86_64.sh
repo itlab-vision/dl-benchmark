63b8192085be75d382ecf28d92e2af525d6b76ec
Kirill Yashin <kerik-yashin@yandex.ru>
2023-10-26 12:21:11 +0300

    Applied comments

--- src/build_scripts/ncnn/build_ncnn_linux_x86_64.sh
+++ src/build_scripts/ncnn/build_ncnn_linux_x86_64.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-WORKSPACE=${WORKSPACE:-"/opt/ncnn_build"}
-mkdir ${WORKSPACE}
-cd ${WORKSPACE}
+# BUILD_DIR - directory for build ncnn (recommended: ${HOME}/ncnn_build)
+# INSTALL_PREFIX - directory for install ncnn (recommended: /usr/local)
+
+BUILD_DIR=${1:-""}
+INSTALL_PREFIX=${2:-""}
+
+if [ -z "$BUILD_DIR" ]; then
+    echo "Error: Please set build directory as first argument"
+    exit 1
+fi
+
+if [ -z "$INSTALL_PREFIX" ]; then
+    echo "Error: Please set install prefix as second argument"
+    exit 1
+fi
+
+mkdir "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
 
 git clone https://github.com/Tencent/ncnn.git
 cd ncnn && git submodule update --init
 
-mkdir build && cd build
+mkdir build && cd build || exit
 cmake -DCMAKE_BUILD_TYPE=Release -DNCNN_VULKAN=ON -DNCNN_SYSTEM_GLSLANG=ON -DNCNN_BUILD_EXAMPLES=ON ..
-make -j8
+make "-j$(nproc)"
 make install
-mkdir /usr/local/lib/ncnn
-cp -r install/include/ncnn /usr/local/include/ncnn && cp -r install/lib/libncnn.a /usr/local/lib/ncnn/libncnn.a
+mkdir "${INSTALL_PREFIX}/lib/ncnn"
+cp -r "install/include/ncnn" "${INSTALL_PREFIX}/include/ncnn" && cp -r "install/lib/libncnn.a" "${INSTALL_PREFIX}/lib/ncnn/libncnn.a"
+printf "PATH=${PATH}:${INSTALL_PREFIX}" | tee -a /etc/bash.bashrc
+printf "PATH=${LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}/lib" | tee -a /etc/bash.bashrc
--- src/build_scripts/ncnn/build_prerequisites_linux_x86_64.sh
+++ src/build_scripts/ncnn/build_prerequisites_linux_x86_64.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-WORKSPACE=${WORKSPACE:-"/opt/ncnn_build"}
-mkdir ${WORKSPACE}
-cd ${WORKSPACE}
+# BUILD_DIR - directory for build prerequisites (recommended: ${HOME}/ncnn_build)
+# INSTALL_PREFIX - directory for install prerequisites (recommended: /usr/local)
+
+BUILD_DIR=${1:-""}
+INSTALL_PREFIX=${2:-""}
+
+if [ -z "$BUILD_DIR" ]; then
+    echo "Error: Please set build directory as first argument"
+    exit 1
+fi
+
+if [ -z "$INSTALL_PREFIX" ]; then
+    echo "Error: Please set install prefix as second argument"
+    exit 1
+fi
+
+mkdir "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
 
 # Install packages
 apt update
@@ -13,20 +28,22 @@ git clone https://github.com/protocolbuffers/protobuf.git -b v4.24.4
 cd protobuf && git submodule update --init --recursive
 
 cmake . -DCMAKE_CXX_STANDARD=14
-cmake --build . --parallel 8
+cmake --build . --parallel "$(nproc)"
 ctest --verbose
 cmake --install .
 
 # Install OpenCV
 
-cd ${WORKSPACE}
-wget https://github.com/opencv/opencv/archive/4.8.0.zip && unzip 4.8.0.zip && cd opencv-4.8.0/
+cd "${BUILD_DIR}" || eixt
+wget https://github.com/opencv/opencv/archive/4.8.0.zip && unzip 4.8.0.zip && cd opencv-4.8.0/ || exit
 
-mkdir build && cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make -j8
+mkdir build && cd build || exit
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ..
+make "-j$(nproc)"
 make install
 
-echo "include /usr/local/lib" | tee -a /etc/ld.so.conf
+echo "include ${INSTALL_PREFIX}/lib" | tee -a /etc/ld.so.conf
 ldconfig
-printf "PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig\nexport PKG_CONFIG_PATH\n" | tee -a /etc/bash.bashrc
+printf "PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${INSTALL_PREFIX}/lib/pkgconfig\nexport PKG_CONFIG_PATH\n" | tee -a /etc/bash.bashrc
+printf "PATH=${PATH}:${INSTALL_PREFIX}" | tee -a /etc/bash.bashrc
+printf "PATH=${LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}/lib" | tee -a /etc/bash.bashrc
