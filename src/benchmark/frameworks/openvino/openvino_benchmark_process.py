3e63f7c400b2f80a1049531258905136b1da1dea
Ilya Naumov <i.naumov@yadro.com>
2023-11-10 16:35:21 +0300

    Pull request #149: [AI-9265] Add latency per token metric for OpenVINO
    Merge in AT/dl-benchmark from inaumov/AI-9265-ov-latency-per-token to develop

--- src/benchmark/frameworks/openvino/openvino_benchmark_process.py
+++ src/benchmark/frameworks/openvino/openvino_benchmark_process.py
@@ -53,12 +53,17 @@ class OpenVINOBenchmarkProcess(OpenVINOProcess):
                                        if None not in (duration, iter_count) else None)
         fps = self._get_benchmark_app_metric('Throughput')
         latency = round(self._get_benchmark_app_metric('Median') / 1000, 5)
+        latency_per_token = 'N/A'
+        if self._test.dep_parameters.num_output_tokens:
+            num_tokens = self._test.dep_parameters.num_output_tokens
+            latency_per_token = round(latency / num_tokens, 5)
+
         metrics = {
             'average_time': average_time_of_single_pass,
             'fps': fps,
             'latency': latency,
             'batch_fps': 0.0,
-            'latency_per_token': 'N/A',
+            'latency_per_token': latency_per_token,
         }
         return metrics
 
@@ -247,12 +252,16 @@ class OpenVINOBenchmarkCppProcess(OpenVINOBenchmarkProcess):
 
         fps = round(float(report['execution_results']['throughput']), 3)
         latency = round(float(report['execution_results']['latency_median']) / MILLISECONDS_IN_SECOND, 5)
+        latency_per_token = 'N/A'
+        if self._test.dep_parameters.num_output_tokens:
+            num_tokens = self._test.dep_parameters.num_output_tokens
+            latency_per_token = round(latency / num_tokens, 5)
         metrics = {
             'average_time': average_time_of_single_pass,
             'fps': fps,
             'latency': latency,
             'batch_fps': 0.0,
-            'latency_per_token': 'N/A',
+            'latency_per_token': latency_per_token,
         }
         return metrics
 
