0108f02143a7cbc85d4500b755afc17d83899d9d
valentina-kustikova <valentina.kustikova@gmail.com>
2023-11-12 00:28:52 +0300

    Fix test

--- test/smoke_test/run_bench_smoke_test_caffe.sh
+++ test/smoke_test/run_bench_smoke_test_caffe.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-conda activate caffe_env_3.7
-
-conda install caffe
-
 . ./utils.sh
 
 omz_downloader --output_dir working_dir_smoke --cache_dir cache_dir_smoke --name=googlenet-v1
@@ -16,7 +12,5 @@ smoke_tests_count=$(grep -io '<Test>' "$smoke_config_file" | wc -l)
 python3 ../../src/benchmark/inference_benchmark.py -r $result_file --executor_type host_machine -c $smoke_config_file
 check_exit_code inference_benchmark
 check_results_file $result_file $smoke_tests_count
-
-conda deactivate
 
 exit $return_value
