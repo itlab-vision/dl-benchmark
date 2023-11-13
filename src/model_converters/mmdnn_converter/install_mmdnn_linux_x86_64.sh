0193a94366bf6260e35c1c6623b8dce6ddf0bda3
Sergey Borisov <66353172+SergeyBoRss@users.noreply.github.com>
2023-11-13 21:10:10 +0300

    Update install_mmdnn_linux_x86_64.sh

--- src/model_converters/mmdnn_converter/install_mmdnn_linux_x86_64.sh
+++ src/model_converters/mmdnn_converter/install_mmdnn_linux_x86_64.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Создание виртуального окружения openvino
-conda create -n openvino
-conda activate openvino
-sudo apt install python3-pip
-
-pip install openvino-dev
-export PATH=$PATH:/home/user/.local/bin
-
 # Создание виртуального окружения mmdnn
 conda create -n mmdnn python=3.6
 conda activate mmdnn
 sudo apt install python3-pip
 
 pip install -r requirements.txt
+conda deactivate
