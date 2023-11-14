f008ab91a0346ed50629cc48912532594ae306a9
Atikin-NT <ivanov.n.a.0620@gmail.com>
2023-11-14 10:37:04 +0300

    add smoke tests (from my git test)

--- test/smoke_test/run_bench_smoke_test.sh
+++ test/smoke_test/run_bench_smoke_test.sh
@@ -5,12 +5,16 @@
 omz_downloader --output_dir working_dir_smoke --cache_dir cache_dir_smoke      --name=mobilenet-v1-1.0-224-tf,efficientnet-b0-pytorch
 omz_converter  --output_dir working_dir_smoke --download_dir working_dir_smoke --name=mobilenet-v1-1.0-224-tf,efficientnet-b0-pytorch --precisions FP32
 
+mkdir -p working_dir_smoke/dgl
+curl -o working_dir_smoke/dgl/model.pt https://raw.githubusercontent.com/Atikin-NT/dgl-test/main/model.pt
+curl -o working_dir_smoke/dgl/GCN.py https://raw.githubusercontent.com/Atikin-NT/dgl-test/main/GCN.py
+
 result_file="results_benchmark.csv"
 [ -f $result_file ] && rm $result_file
 smoke_config_file="smoke_config.xml"
 smoke_tests_count=$(grep -io '<Test>' "$smoke_config_file" | wc -l)
 
-python3 ../../src/benchmark/inference_benchmark.py -r $result_file --executor_type host_machine -c $smoke_config_file
+python ../../src/benchmark/inference_benchmark.py -r $result_file --executor_type host_machine -c $smoke_config_file
 check_exit_code inference_benchmark
 check_results_file $result_file $smoke_tests_count
 
