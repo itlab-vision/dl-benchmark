ef265524c2944d8167d747f17175eb99495a96fa
Atikin-NT <ivanov.n.a.0620@gmail.com>
2023-11-15 17:52:48 +0300

    add itlab repo for tests

--- test/smoke_test/run_bench_smoke_test.sh
+++ test/smoke_test/run_bench_smoke_test.sh
@@ -6,8 +6,8 @@ omz_downloader --output_dir working_dir_smoke --cache_dir cache_dir_smoke      -
 omz_converter  --output_dir working_dir_smoke --download_dir working_dir_smoke --name=mobilenet-v1-1.0-224-tf,efficientnet-b0-pytorch --precisions FP32
 
 mkdir -p working_dir_smoke/dgl
-curl -o working_dir_smoke/dgl/model.pt https://raw.githubusercontent.com/Atikin-NT/dgl-test/main/model.pt
-curl -o working_dir_smoke/dgl/GCN.py https://raw.githubusercontent.com/Atikin-NT/dgl-test/main/GCN.py
+curl -o working_dir_smoke/dgl/model.pt https://raw.githubusercontent.com/itlab-vision/dgl/main/models/model.pt
+curl -o working_dir_smoke/dgl/GCN.py https://raw.githubusercontent.com/itlab-vision/dgl/main/models/GCN.py
 
 result_file="results_benchmark.csv"
 [ -f $result_file ] && rm $result_file
