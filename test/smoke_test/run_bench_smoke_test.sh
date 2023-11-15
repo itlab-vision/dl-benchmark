c6b6446da853298f65f353e4dace31a36eae03e9
Atikin-NT <ivanov.n.a.0620@gmail.com>
2023-11-15 18:07:29 +0300

    sh fix

--- test/smoke_test/run_bench_smoke_test.sh
+++ test/smoke_test/run_bench_smoke_test.sh
@@ -14,7 +14,7 @@ result_file="results_benchmark.csv"
 smoke_config_file="smoke_config.xml"
 smoke_tests_count=$(grep -io '<Test>' "$smoke_config_file" | wc -l)
 
-python ../../src/benchmark/inference_benchmark.py -r $result_file --executor_type host_machine -c $smoke_config_file
+python3 ../../src/benchmark/inference_benchmark.py -r $result_file --executor_type host_machine -c $smoke_config_file
 check_exit_code inference_benchmark
 check_results_file $result_file $smoke_tests_count
 
