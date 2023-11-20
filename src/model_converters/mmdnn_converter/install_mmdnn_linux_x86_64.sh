906d37c8bf69a5624109ead59cdc4f7ec9032797
Sergey Borisov <66353172+SergeyBoRss@users.noreply.github.com>
2023-11-20 08:03:30 +0300

    Update pip install

--- src/model_converters/mmdnn_converter/install_mmdnn_linux_x86_64.sh
+++ src/model_converters/mmdnn_converter/install_mmdnn_linux_x86_64.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Создание виртуального окружения mmdnn
-conda create -n mmdnn python=3.6
-conda activate mmdnn
-sudo apt install python3-pip
+conda create -n mmdnn1 python=3.6
+conda activate mmdnn1
+conda install pip
 
 pip install -r requirements.txt
 conda deactivate
