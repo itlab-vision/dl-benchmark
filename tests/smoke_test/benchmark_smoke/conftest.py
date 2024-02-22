04a8dac9a854153f08739c594600f85b89d76c82
ArchiMikael <archimikael@gmail.com>
2024-02-22 16:41:29 +0300

    Fixed copy mistake

--- tests/smoke_test/benchmark_smoke/conftest.py
+++ tests/smoke_test/benchmark_smoke/conftest.py
@@ -142,29 +142,6 @@ def convert_models_to_tvm(use_caffe: bool = False):
         except ImportError:
             log.info('No tvm module found, skip tvm converters')
 
-def convert_models_to_tvm(use_caffe: bool = False):
-    pytorch_to_tvm_converter = (f'cd {OUTPUT_DIR} && python3 {TVM_CONVERTER} -mn efficientnet_b0 '
-                                f'-w {OUTPUT_DIR}/public/efficientnet-b0-pytorch/efficientnet-b0.pth '
-                                f'-is 1 3 224 224 -f pytorch -op {OUTPUT_DIR}/public/efficientnet-b0-pytorch/')
-    mxnet_to_tvm_converter = f'cd {OUTPUT_DIR} && python3 {TVM_CONVERTER} -mn alexnet -is 1 3 224 224 -f mxnet'
-    caffe_to_tvm_converter = (f'cd {OUTPUT_DIR} && python3 {TVM_CONVERTER} -mn googlenet-v1 -is 1 3 224 224 '
-                              f'-m {OUTPUT_DIR}/public/googlenet-v1/googlenet-v1.prototxt '
-                              f'-w {OUTPUT_DIR}/public/googlenet-v1/googlenet-v1.caffemodel '
-                              f'-op {OUTPUT_DIR}/public/googlenet-v1/ -f caffe')
-    tvm_compiler = (f'cd {OUTPUT_DIR} && python3 {TVM_COMPILER} -m alexnet.json '
-                    '-p alexnet.params -t llvm --lib_name alexnet_vm.so -vm')
-
-    if use_caffe:
-        execute_process(command_line=caffe_to_tvm_converter, log=log)
-    else:
-        try:
-            import tvm  # noqa: F401
-            execute_process(command_line=pytorch_to_tvm_converter, log=log)
-            execute_process(command_line=mxnet_to_tvm_converter, log=log)
-            execute_process(command_line=tvm_compiler, log=log)
-        except ImportError:
-            log.info('No tvm module found, skip tvm converters')
-
 
 @pytest.fixture(scope='session', autouse=True)
 def prepare_dl_models(request, overrided_models):
