c03422507db774eecd2085aaaab680c998dd18fe
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-05-11 01:00:42 +0300

    Update convert_onnx_to_tf.py

--- src/model_converters/tf2onnx/convert_onnx_to_tf.py
+++ src/model_converters/tf2onnx/convert_onnx_to_tf.py
@@ -19,6 +19,24 @@ def cli_argument_parser():
                         required=True,
                         type=str,
                         dest='output')
+    parser.add_argument('--output_keras_v3',
+                        help='Output like keras_v3 model. True or False.',
+                        type=str,
+                        dest='output_keras_v3')
+    parser.add_argument('--output_tfv1_pb',
+                        help='Output like tfv1_model model. True or False.',
+                        type=str,
+                        dest='output_tfv1_pb')
+    parser.add_argument('--input_shape',
+                        help='Shape of the input tensor as a Python list. '
+                             'Should be in the format "[batch_size, height, width, channels]".',
+                        required=True,
+                        type=str,
+                        dest='input_shape')
+    parser.add_argument('--input_name',
+                        help='Name of the input tensor',
+                        type=str,
+                        dest='input_name')       
 
     args = parser.parse_args()
     return args
@@ -33,15 +51,18 @@ def main():
             input_onnx_file_path=args.onnx_model,
             output_folder_path=args.output,
             copy_onnx_input_output_names_to_tflite=True,
-            non_verbose=True,
+            non_verbose=False,
+            output_tfv1_pb=args.output_tfv1_pb,
+            output_keras_v3=args.output_keras_v3,
+            overwrite_input_shape=[''.join(args.input_name.split()+':'.split(' ') + args.input_shape.split(' '))],
         )
 
         # Print conversion success message and output path
-        logger.info('Model successfully converted to ONNX.')
-        logger.info(f'ONNX model saved at: {args.output}')
+        logger.info('Model successfully converted to TF.')
+        logger.info(f'TF model saved at: {args.output}')
     except Exception as e:
         # If conversion fails, print error message
-        logger.error(f'Conversion to ONNX failed: {e}')
+        logger.error(f'Conversion to TF failed: {e}')
 
     
 if __name__ == '__main__':
