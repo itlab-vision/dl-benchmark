7628abfd0b56b3a5b6cd860de63e42aa70cf68ce
igkonov <igkonov@yandex.ru>
2024-10-27 01:27:34 +0300

    line length fix

--- src/benchmark/frameworks/paddlepaddle/paddlepaddle_process.py
+++ src/benchmark/frameworks/paddlepaddle/paddlepaddle_process.py
@@ -54,7 +54,8 @@ class PaddlePaddleProcess(ProcessHandler):
         common_params = self._add_optional_argument_to_cmd_line(common_params, '--output_names', output_names)
 
         gpu_mem_size = self._test.dep_parameters.gpu_mem_size
-        common_params = self._add_optional_argument_to_cmd_line(common_params, '--memory_pool_init_size_mb', gpu_mem_size)
+        common_params = (
+            self._add_optional_argument_to_cmd_line(common_params, '--memory_pool_init_size_mb', gpu_mem_size))
 
         common_params = self._add_argument_to_cmd_line(common_params, '--raw_output', 'true')
 
