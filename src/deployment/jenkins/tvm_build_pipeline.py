3bf8369d8fa77eeac0c96182bf9195d6f3cb2579
ismukhin <vvstanka39@gmail.com>
2024-12-06 21:23:21 +0300

    fix

--- src/deployment/jenkins/tvm_build_pipeline.py
+++ src/deployment/jenkins/tvm_build_pipeline.py
@@ -5,14 +5,19 @@ import subprocess
 
 
 class TVMBuilder:
-    def __init__(self, branch, conda):
+    def __init__(self, branch, conda, py):
         self.branch = branch
         self.conda = conda
+        self.py_version = py
 
     def _run(self, cmd):
         return subprocess.run(cmd, shell=True)
 
     def build_tvm(self):
+        self._run(f'{self.conda_prefix}/bin/conda create -y -n tvm_main python=={self.py_version}')
+        self._run(f'{self.conda_prefix}/bin/conda install -n tvm_main -c conda-forge -y gcc=12.1.0')
+        self._run(f'{self.conda_prefix}/bin/conda install -n tvm_main -c conda-forge -y gxx_linux-64')
+        self._run(f'{self.conda_prefix}/envs/tvm_main/bin/pip3 install -r requirements.txt')
         self._run(f'git clone --recursive https://github.com/apache/tvm -b {self.branch}')
         self._run(f'cd tvm && mkdir -p build && cd build && cmake -DUSE_LLVM=ON ../ && make -j$(nproc --all) && cd ../python && {self.conda}/envs/tvm_main/bin/python setup.py install --user')
 
@@ -23,6 +28,11 @@ def cli_arguments_parse():
     parser.add_argument('-b', '--branch',
                         help='Branch to build tvm.',
                         dest='branch',
+                        required=True,
+                        type=str)
+    parser.add_argument('-p', '--python_version',
+                        help='Python version to create conda env.',
+                        dest='py',
                         required=True,
                         type=str)
     parser.add_argument('-cp', '--conda_prefix',
@@ -36,7 +46,7 @@ def cli_arguments_parse():
 
 def main():
     args = cli_arguments_parse()
-    cr = TVMBuilder(args.branch, args.conda)
+    cr = TVMBuilder(args.branch, args.conda, args.py)
     cr.build_tvm()
 
 
