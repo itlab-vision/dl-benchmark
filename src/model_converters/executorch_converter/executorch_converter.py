6d106d513c735295849cd1cd31cbe11cb4e43a97
ismukhin <vvstanka39@gmail.com>
2025-03-27 13:17:20 +0300

    benchmark support and raw model converter

---
+++ src/model_converters/executorch_converter/executorch_converter.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision.models as models
+from torchvision.models.mobilenetv2 import MobileNet_V2_Weights
+from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
+from executorch.exir import to_edge_transform_and_lower
+
+mobilenet_v2 = models.mobilenetv2.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT).eval()
+sample_inputs = (torch.randn(1, 3, 224, 224), )
+
+et_program = to_edge_transform_and_lower(
+    torch.export.export(mobilenet_v2, sample_inputs),
+    partitioner=[XnnpackPartitioner()],
+).to_executorch()
+
+with open("mv2_xnnpack.pte", "wb") as file:
+    et_program.write_to_file(file)
