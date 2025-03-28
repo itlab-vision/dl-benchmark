547299baf52ae6d217b4a287500cac07baf0fcbe
Ivan Mukhin <92549376+ismukhin@users.noreply.github.com>
2025-03-28 10:57:09 +0300

    [C++][Benchmark] Executorch pipeline (#567)
    Co-authored-by: Valentina <valentina-kustikova@users.noreply.github.com>

---
+++ src/build_scripts/ExecuTorch/build_executorch_x86_64.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# BUILD_DIR - directory for build ExecuTorch
+# INSTALL_PREFIX - directory for install ExecuTorch
+
+BUILD_DIR=${1:-""}
+INSTALL_PREFIX=${2:-""}
+
+if [ -z "$BUILD_DIR" ]; then
+    echo "Error: Please set build directory as first argument"
+    exit 1
+fi
+
+if [ -z "$INSTALL_PREFIX" ]; then
+    echo "Error: Please set install prefix as second argument"
+    exit 1
+fi
+
+echo "Creating build directory: ${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
+
+echo "Creating conda environment"
+conda create -y -n executorch_env python=3.10
+conda activate executorch_env
+
+echo "Clone repository"
+git clone -b v0.5.0 https://github.com/pytorch/executorch
+cd executorch && git submodule update --init
+
+echo "Install requirements"
+./install_requirements.sh
+
+echo "Building and install ExecuTorch"
+mkdir build1 && cd build1
+cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+      -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
+      -DEXECUTORCH_BUILD_EXTENSION_RUNNER_UTIL=ON \
+      -DEXECUTORCH_BUILD_EXTENSION_TENSOR=ON \
+      -DEXECUTORCH_BUILD_DEVTOOLS=ON \
+      -DBUILD_EXECUTORCH_PORTABLE_OPS=ON \
+      -DEXECUTORCH_BUILD_XNNPACK=ON ../
+make "-j$(nproc)"
+make install
+
+echo "Deactivating conda environment"
+conda deactivate
---
+++ src/cpp_dl_benchmark/executorch_launcher/include/executorch_launcher.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "common_launcher/launcher.hpp"
+#include "inputs_preparation/tensor_utils.hpp"
+#include "utils/logger.hpp"
+#include "utils/utils.hpp"
+
+#include <executorch/extension/module/module.h>
+#include <executorch/extension/tensor/tensor.h>
+#include <executorch/extension/threadpool/cpuinfo_utils.h>
+#include <executorch/extension/threadpool/threadpool.h>
+
+#include <chrono>
+#include <cstdint>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+class ExecuTorchLauncher : public Launcher {
+public:
+    ExecuTorchLauncher(const int nthreads, const int fps, const std::string& device);
+    virtual ~ExecuTorchLauncher();
+
+    std::string get_framework_name() const override;
+    std::string get_framework_version() const override;
+    std::string get_backend_name() const override;
+
+    void read(const std::string& model_file, const std::string& weights_file = "") override;
+    void load() override{};
+
+    void fill_inputs_outputs_info() override;
+    IOTensorsInfo get_io_tensors_info() const override;
+
+    void prepare_input_tensors(std::vector<std::vector<TensorBuffer>>&& tensor_buffers) override;
+    void compile() override;
+
+    std::vector<OutputTensors> get_output_tensors() override;
+
+private:
+    std::shared_ptr<executorch::extension::Module> module;
+
+    std::vector<std::string> input_names;
+    std::vector<std::vector<int>> input_shapes;
+
+    std::vector<std::string> output_names;
+    std::vector<std::vector<int>> output_shapes;
+
+    std::vector<std::vector<executorch::runtime::EValue>> tensors;
+
+    void run(const int input_idx);
+};
