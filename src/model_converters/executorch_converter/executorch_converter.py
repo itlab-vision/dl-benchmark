ba86c1af350ca403ae9773cc2b00ec3ed4fefc1d
ismukhin <vvstanka39@gmail.com>
2025-04-03 22:45:12 +0300

    converter

--- src/model_converters/executorch_converter/executorch_converter.py
+++ src/model_converters/executorch_converter/executorch_converter.py
@@ -1,16 +1,76 @@
+import sys
+import argparse
 import torch
-import torchvision.models as models
-from torchvision.models.mobilenetv2 import MobileNet_V2_Weights
+from torchvision.models import get_model
 from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
+from executorch.backends.vulkan.partitioner.vulkan_partitioner import VulkanPartitioner
 from executorch.exir import to_edge_transform_and_lower
 
-mobilenet_v2 = models.mobilenetv2.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT).eval()
-sample_inputs = (torch.randn(1, 3, 224, 224), )
 
-et_program = to_edge_transform_and_lower(
-    torch.export.export(mobilenet_v2, sample_inputs),
-    partitioner=[XnnpackPartitioner()],
-).to_executorch()
+PARTITIONER = {
+    'xnnpack': XnnpackPartitioner(),
+    'vulkan': VulkanPartitioner(),
+    'None': None,
+}
 
-with open("mv2_xnnpack.pte", "wb") as file:
-    et_program.write_to_file(file)
+
+def cli_argument_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-mn', '--model_name',
+                        help='Model name from the module.',
+                        required=True,
+                        type=str,
+                        dest='model_name')
+    parser.add_argument('-w', '--weights',
+                        help='Name of weights to use from module',
+                        type=str,
+                        default='DEFAULT',
+                        dest='weights')
+    parser.add_argument('-is', '--input_shapes',
+                        help='Input tensor shapes',
+                        required=True,
+                        type=int,
+                        nargs=4,
+                        dest='input_shapes')
+    parser.add_argument('-b', '--batch_size',
+                        help='Batch size.',
+                        default=1,
+                        type=int,
+                        dest='batch_size')
+    parser.add_argument('-p', '--partitioner',
+                        help='Partitioner to export model',
+                        default='None',
+                        choices=['xnnpack', 'vulkan', 'arm', None],
+                        type=str,
+                        dest='partitioner')
+    args = parser.parse_args()
+
+    return args
+
+
+def export_to_executorch(model_name, weights,
+                         input_shape, partitioner, batch):
+    print(input_shape)
+    model = get_model(model_name, weights=weights)
+    model = model.eval()
+    sample_inputs = (torch.randn(list(input_shape)), )
+
+    et_program = to_edge_transform_and_lower(
+        torch.export.export(model, sample_inputs),
+        partitioner=[PARTITIONER[partitioner]],
+    ).to_executorch()
+
+    with open(f'{model_name}_{partitioner}_batch_{batch}.pte', 'wb') as file:
+        et_program.write_to_file(file)
+
+
+def main():
+    args = cli_argument_parser()
+    export_to_executorch(args.model_name, args.weights,
+                         args.input_shapes, args.partitioner,
+                         args.batch_size)
+
+
+if __name__=='__main__':
+    sys.exit(main() or 0)
