9a413d8577005c0c407a419e3d429e9b77b1873e
ismukhin <vvstanka39@gmail.com>
2025-04-05 13:17:41 +0300

    fixes, add logger

--- src/model_converters/executorch_converter/executorch_converter.py
+++ src/model_converters/executorch_converter/executorch_converter.py
@@ -1,11 +1,16 @@
 import sys
 import argparse
 import torch
+from pathlib import Path
 from torchvision.models import get_model
 from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
 from executorch.backends.vulkan.partitioner.vulkan_partitioner import VulkanPartitioner
 from executorch.exir import to_edge_transform_and_lower
 
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+from utils.logger_conf import configure_logger  # noqa: E402
+
+log = configure_logger()
 
 PARTITIONER = {
     'xnnpack': XnnpackPartitioner(),
@@ -23,12 +28,12 @@ def cli_argument_parser():
                         type=str,
                         dest='model_name')
     parser.add_argument('-w', '--weights',
-                        help='Name of weights to use from module',
+                        help='Name of weights to use from module.',
                         type=str,
                         default='DEFAULT',
                         dest='weights')
     parser.add_argument('-is', '--input_shapes',
-                        help='Input tensor shapes',
+                        help='Input tensor shapes.',
                         required=True,
                         type=int,
                         nargs=4,
@@ -39,7 +44,7 @@ def cli_argument_parser():
                         type=int,
                         dest='batch_size')
     parser.add_argument('-p', '--partitioner',
-                        help='Partitioner to export model',
+                        help='Partitioner to export model.',
                         default='None',
                         choices=['xnnpack', 'vulkan', 'arm', None],
                         type=str,
@@ -51,26 +56,30 @@ def cli_argument_parser():
 
 def export_to_executorch(model_name, weights,
                          input_shape, partitioner, batch):
-    print(input_shape)
+    log.info(f'Getting the model {model_name} from torchvision.')
     model = get_model(model_name, weights=weights)
     model = model.eval()
     sample_inputs = (torch.randn(list(input_shape)), )
 
+    log.info('Export model to ExecuTorch format.')
     et_program = to_edge_transform_and_lower(
         torch.export.export(model, sample_inputs),
-        partitioner=[PARTITIONER[partitioner]],
+        partitioner=PARTITIONER[partitioner],
     ).to_executorch()
 
-    with open(f'{model_name}_{partitioner}_batch_{batch}.pte', 'wb') as file:
+    save_name = f'{model_name}_{partitioner}_batch_{batch}.pte'
+    log.info(f'Save model as {save_name}.')
+    with open(save_name, 'wb') as file:
         et_program.write_to_file(file)
 
 
 def main():
+    log.info('Parsing command line arguments.')
     args = cli_argument_parser()
     export_to_executorch(args.model_name, args.weights,
                          args.input_shapes, args.partitioner,
                          args.batch_size)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     sys.exit(main() or 0)
